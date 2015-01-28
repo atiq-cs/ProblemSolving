4ebd7455b09d173117d272a2c7909260e927d8f8
Atiqur Rahman <mdarahman@cs.stonybrook.edu>
2015-01-28 06:32:51 -0500

    Addition in template project - template version 2

--- coding-template/cpp/Template.cpp
+++ coding-template/cpp/Template.cpp
@@ -1,35 +1,72 @@
-/*******************************************************
-*		Problem Name:
-*		Problem ID:
-*		Occassion:				_ Contest _ _ _
-*
-*		Algorithm:
-*		Special Case:
-*		Judge Status:
-*		Author:					Saint Atique
-*		Notes:					from F:\Sourcecodes\Problems Trying Now
-*
-*******************************************************/
+/*
+ *	Problem Title:	Problem Title
+*	Problem URL:	Problem Link
+ *	Problem Type:
+ *	Alogirthm	:
+ *	Coder		:	Atiqur Rahman
+ *	Desc		:	template
+ *
+ *
+ *
+ *
+ *
+ *	Status		:	..
+ */
 
-//Uncomment this line if you use and standard IO function
-//#include <cstdio>
-//#include <iostream>
+#include <iostream>
+#include <string>
+#include <sstream>
 #include <cmath>
-#include <cstring>
-//#include <new>
-#include <vector>
-#include <queue>
-#include <map>
-#include <algorithm>
-#include <iomanip>//for cout formatting
-#define	INF 2147483648
-#define EPS 1e-8
+//#define FILE_IO	TRUE
+
+#ifdef FILE_IO
+#include <fstream>
+#endif
 using namespace std;
 
-int main() {
-	freopen("_in.txt", "r", stdin);
-	freopen("_out.txt", "w", stdout);
+class ClassName;
+void handleIO();
 
-
+int mainT() {
+	handleIO();
 	return 0;
 }
+
+void handleIO() {
+#ifdef FILE_IO
+	ofstream outFile("ProblemNo_out.txt");
+	streambuf *psbuf = outFile.rdbuf(), *backup;
+	backup = cout.rdbuf();     // back up cout's streambuf
+	cout.rdbuf(psbuf);
+#endif
+
+	ClassName classObj;
+	string varName;
+
+	while (cin >> varName) {
+		classObj.initClass();
+	}
+#ifdef FILE_IO
+	cout.rdbuf(backup);
+	outFile.close();
+#endif
+}
+
+class ClassName {
+private:
+	string str;
+	int num;
+public:
+	void initClass();
+};
+
+void ClassName::initClass() {
+	num = 0;
+}
+
+/* example: how to set fixed pionts
+memset(arrayName, 255, )
+cout.setf (ios::fixed, ios::floatfield);
+cout.setf(ios::showpoint);
+cout<<setprecision(2)<<sum_c + eps<<endl;
+*/
