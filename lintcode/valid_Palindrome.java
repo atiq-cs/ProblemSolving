7e6fb4519ed80b5786328bf07bc6c4ebd37c8da5
tianshubao1 <baotianshu90@163.com>
2015-06-01 16:59:59 -0400

    Added online problem solving sources
    lintcode/valid_Palindrome.java
    	-Given a string, determine if it is a palindrome, considering only
    	alphanumeric characters and ignoring cases

---
+++ lintcode/valid_Palindrome.java
@@ -0,0 +1,27 @@
+public class Solution {
+    /**
+     * @param s A string
+     * @return Whether the string is a valid palindrome
+     */
+    public boolean isPalindrome(String s) {//solution from leetcode book, many details problems
+        // Write your code here
+        if(s.length() == 0 || s == null)
+            return true;
+        
+        int start = 0, end = s.length() - 1;
+        
+        while(start < end){
+            while(start < end && !Character.isLetterOrDigit(s.charAt(start)))//number is included
+                start++;
+            while(start < end && !Character.isLetterOrDigit(s.charAt(end)))
+                end--;
+            if(Character.toLowerCase(s.charAt(start)) !=
+                Character.toLowerCase(s.charAt(end)))
+                return false;
+            start++;
+            end--;
+        }
+        
+        return true;
+    }
+}
