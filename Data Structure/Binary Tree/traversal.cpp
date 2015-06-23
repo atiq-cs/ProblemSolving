e56a410a922b66e6d22169d2094cc9bb60ae1c21
Atiqur Rahman <mdarahman@cs.stonybrook.edu>
2015-06-22 23:58:06 -0400

    Data Structure/Binary Tree/traversal.cpp  - Though this solves hackerrank problems  - We also solved lintcode traversal problems almost same code

---
+++ Data Structure/Binary Tree/traversal.cpp
@@ -0,0 +1,54 @@
+/*
+    Problem     : 3 types of Traversal
+    Description :
+                  https://en.wikipedia.org/wiki/Tree_traversal
+                   Pre-order
+                   In-order
+                   Post-order
+                  Tested solving hackerrank problems on the topic
+
+    Complexity  : O(n)
+
+    Status      :   Accepted
+*/
+
+/*
+    Pre-order traversal of Binary Tree
+    Solves https://www.hackerrank.com/challenges/tree-preorder-traversal
+*/
+
+void Preorder(node *root) {
+    if (root == NULL)
+        return;
+
+    std::cout << root->data << " ";
+
+    Preorder(root->left);
+    Preorder(root->right);
+}
+
+/*
+    In-order traversal of Binary Tree
+    Solves https://www.hackerrank.com/challenges/tree-inorder-traversal/
+*/
+void Inorder(node *root) {
+    if (root == NULL)
+        return;
+
+    Inorder(root->left);
+    std::cout << root->data << " ";
+    Inorder(root->right);
+}
+
+/*
+    Post-order traversal of Binary Tree
+    Solves https://www.hackerrank.com/challenges/tree-postorder-traversal/
+*/
+void Postorder(node *root) {
+    if (root == NULL)
+        return;
+
+    Postorder(root->left);
+    Postorder(root->right);
+    std::cout << root->data << " ";
+}
