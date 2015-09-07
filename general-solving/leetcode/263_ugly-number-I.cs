6f790d8e040dcb976e3da481ab5364015b8c7da8
Atiq Rahman <atiqcx@gmail.com>
2015-09-06 21:56:28 -0400

    ugly number I solution added

---
+++ general-solving/leetcode/263_ugly-number-I.cs
@@ -0,0 +1,25 @@
+/***************************************************************************
+*   Problem Name:   Find the minimum depth of a binary tree
+*   Problem URL :   https://leetcode.com/problems/ugly-number/
+*
+*   Complexity  :   how to compute complexity of this
+*   Author      :   Atiq Rahman
+*   Status      :   Accepted
+*   Notes       :   
+***************************************************************************/
+
+public class Solution
+{
+    public bool IsUgly(int num)
+    {
+        if (num == 0)
+            return false;
+
+        int[] divs = { 2, 3, 5 };
+        for (int i = 0; i < 3; i++)
+            while (num % divs[i] == 0)
+                num /= divs[i];
+
+        return num == 1;
+    }
+}
