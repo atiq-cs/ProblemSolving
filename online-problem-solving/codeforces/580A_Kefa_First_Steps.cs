7457381f870bacf947dd118c37243bacda128e98
Atiq Rahman <atiqcx@gmail.com>
2016-11-26 20:45:37 -0800

    Added a simple DP solution to 580A

---
+++ online-problem-solving/codeforces/580A_Kefa_First_Steps.cs
@@ -0,0 +1,36 @@
+﻿/***************************************************************************
+*   Problem Name: Kefa and First Steps
+*   Problem URL : http://codeforces.com/problemset/problem/580/A
+*   Date        : Nov 26, 2016
+*   Occasion    : Codeforces Round #321 (Div. 2)
+*   Complexity  : O(n)
+*   Author      : Atiq Rahman
+*   Status      : Accepted (77ms)
+*   Notes       : Linear DP, not LIS variation at all
+*   meta        : tag-dp
+***************************************************************************/
+using System;
+
+public class Demo {
+    static uint run_lineardp(uint[] a, uint n) {
+        uint cur_seq_len = 1;
+        uint max_seq_len = 0;
+        for (int i = 1; i < n; i++)
+            if (a[i] >= a[i - 1])
+                cur_seq_len++;
+            else {
+                max_seq_len = Math.Max(max_seq_len, cur_seq_len);
+                cur_seq_len = 1;
+            }
+        return Math.Max(max_seq_len, cur_seq_len);
+    }
+
+    public static void Main() {
+        uint n = uint.Parse(Console.ReadLine());
+        string[] tokens = Console.ReadLine().Split();
+        uint[] a = new uint[n];
+        for (int i = 0; i < n; i++)
+            a[i] = uint.Parse(tokens[i]);
+        Console.WriteLine(run_lineardp(a,n));
+    }
+}
