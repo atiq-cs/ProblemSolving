0cd0555c519a498cc5ae4f29c06db06f19f8a8bd
Atiq Rahman <atiqcx@gmail.com>
2016-12-20 11:37:16 -0800

    one liner for simple problem and using hackerrank style IO

--- general-solving/hackerrank/algo/warmup/002_simple-array-sum.cs
+++ general-solving/hackerrank/algo/warmup/002_simple-array-sum.cs
@@ -11,8 +11,19 @@
 *   meta        :   tag-easy
 ***************************************************************************/
 using System;
+using System.Linq;
 
-class Solution {
+class HK_Solution
+{
+    // HackerRank style C# IO
+    static void Main(String[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        string[] arr_temp = Console.ReadLine().Split(' ');
+        int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+        Console.WriteLine(arr.Sum()); // linq statement
+    }
+    /* The regular IO I use
     static void Main(String[] args) {
         int N = int.Parse(Console.ReadLine());
 
@@ -23,5 +34,5 @@ class Solution {
             sum += num;
         }
         Console.WriteLine(sum);
-    }
+    }*/
 }
