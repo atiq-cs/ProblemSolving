99151c1013097de377d6ead7f6494ec7dab7385d
Atiq Rahman <atiqcx@gmail.com>
2017-01-21 18:05:56 -0800

    Addition of a contest dir for codeforces with the addition of an ICPC contest problem: Adjustment Office

---
+++ online-problem-solving/codeforces/contests/NEERC-15/A_Adjustment_Office.cs
@@ -0,0 +1,75 @@
+/***************************************************************************
+* Problem Name: Adjustment Office
+* Problem URL : http://codeforces.com/gym/100851/attachments/download/3961/\
+20152016-acmicpc-northeastern-european-regional-contest-neerc-15-en.pdf
+* Date        : Jan 22, 2016
+* Complexity  : O() Time
+* Author      : Atiq Rahman
+* Status      : Accepted (108ms)
+* Occasion    : 2015-2016 ACM-ICPC Northeastern European Regional Contest
+* Notes       : 
+*               fill in ...
+*               
+* meta        : tag-acm-icpc
+***************************************************************************/
+
+using System;
+
+public class MatQuerySolution {
+    long n;
+    bool[] row_set;
+    bool[] col_set;
+    long ColZeroSum;
+    long RowZeroSum;
+    long nColZero;
+    long nRowZero;
+
+    public MatQuerySolution(int nn) {
+        n = nn;
+        row_set = new bool[n];
+        col_set = new bool[n];
+    }
+
+    public long RowQuery(long i) {
+        if (row_set[i-1])
+            return 0;
+        long sum = GetPartialSeriesSum(i) - ColZeroSum - i * nColZero;
+        RowZeroSum += i; nRowZero++; row_set[i-1] = true;
+        return sum;
+    }
+    public long ColumnQuery(long i) {
+        if (col_set[i-1])
+            return 0;
+        long sum = GetPartialSeriesSum(i) - RowZeroSum - i * nRowZero;
+        ColZeroSum += i; nColZero++;  col_set[i-1] = true;
+        return sum;
+    }
+
+    public long GetPartialSeriesSum(long t) {
+        return n * (n + 2*t + 1) / 2;
+    }
+}
+
+public class Demo {
+    public static void Main() {
+        System.IO.StreamReader inFile = new System.IO.StreamReader("adjustment.in");
+        string[] tokens = inFile.ReadLine().Split();
+        int n = int.Parse(tokens[0]);
+        int q = int.Parse(tokens[1]);
+        MatQuerySolution matSol = new MatQuerySolution(n);
+        System.IO.StreamWriter outFile = new System.IO.StreamWriter("adjustment.out");
+
+        for (int i = 0; i < q; i++) {
+            tokens = inFile.ReadLine().Split();
+            char p = char.Parse(tokens[0]);
+            int t = int.Parse(tokens[1]);
+
+            if (p == 'R')
+                outFile.WriteLine(matSol.RowQuery(t));
+            else if (p == 'C')
+                outFile.WriteLine(matSol.ColumnQuery(t));
+        }
+        inFile.Close();
+        outFile.Close();
+    }
+}
