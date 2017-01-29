1a6a9914c9c9e81f6025373e5220d011c31fa231
Atiq Rahman <atiqcx@gmail.com>
2017-01-28 18:22:15 -0800

    Hackerrank Dynamic Programming Section Solution numbers updated In section cracking-the-coding-interview/data-structure making anagrams added

---
+++ general-solving/hackerrank/Cracking_the_Coding_Interview_Challenges/Data Structures/002_making-anagrams.cs
@@ -0,0 +1,37 @@
+﻿/***************************************************************************
+* Problem Name: Strings: Making Anagrams
+* Problem URL : https://www.hackerrank.com/challenges/ctci-making-anagrams
+* Date        : Jan 22, 2016
+* Complexity  : O(n+m) Time
+* Author      : Atiq Rahman
+* Status      : Accepted
+* Notes       : 
+*               Find frequencies of chars
+*               Get differences and then sum
+*               
+* meta        : tag-easy; tag-string
+***************************************************************************/
+using System;
+
+class Solution {
+    static void Main(String[] args) {
+        string a = Console.ReadLine();
+        string b = Console.ReadLine();
+        
+        int[] fa= new int['z'+1];
+        int[] fb= new int['z'+1];
+        getFrequency(a, fa);
+        getFrequency(b, fb);
+        
+        int dSum = 0;
+        for (int i='0'; i<='z'; i++)
+            dSum += Math.Abs(fa[i]-fb[i]);
+            
+        Console.WriteLine(dSum);
+    }
+    
+    static void getFrequency(string s, int[] f) {
+        for (int i=0; i<s.Length; i++)
+            f[s[i]]++;
+    }
+}
