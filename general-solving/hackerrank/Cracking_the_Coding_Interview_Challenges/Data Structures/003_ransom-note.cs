3b0683071e9f9a5baeaff2c739c18526c2fbe1ae
Atiq Rahman <atiqcx@gmail.com>
2017-01-28 18:40:56 -0800

    Two more solution added under section cracking-the-coding-interview/data-structure

---
+++ general-solving/hackerrank/Cracking_the_Coding_Interview_Challenges/Data Structures/003_ransom-note.cs
@@ -0,0 +1,57 @@
+﻿/***************************************************************************
+* Problem Name: Hash Tables: Ransom Note
+* Problem URL : https://www.hackerrank.com/challenges/ctci-ransom-note
+* Date        : Jan 23, 2016
+* Complexity  : O(n+m) Time
+* Author      : Atiq Rahman
+* Status      : Accepted
+* Notes       : 
+*               The passing of m and n is redundant
+*               We could use Array.Length
+*               
+* meta        : tag-hashtable; tag-data-structure
+***************************************************************************/
+using System;
+using System.Collections.Generic;
+
+class Solution {
+  static void Main(String[] args) {
+    string[] tokens_m = Console.ReadLine().Split(' ');
+    int m = Convert.ToInt32(tokens_m[0]);
+    int n = Convert.ToInt32(tokens_m[1]);
+    string[] magazine = Console.ReadLine().Split(' ');
+    string[] ransom = Console.ReadLine().Split(' ');
+
+    Dictionary<string, int> wordDict = new Dictionary<string, int>();
+    GetWordDict(magazine, m, wordDict);
+    if (CreateRansomReplica(ransom, n, wordDict))
+      Console.WriteLine("Yes");
+    else
+      Console.WriteLine("No");
+  }
+
+  /* fill in the hashset with words from magazine */
+  static void GetWordDict(string[] arr, int len, Dictionary<string, int> wordDict) {
+    for (int i = 0; i < len; i++)
+      if (wordDict.ContainsKey(arr[i]))
+        wordDict[arr[i]]++;
+      else
+        wordDict.Add(arr[i], 1);
+  }
+
+  /*
+    Assumption: input magazine word set is more treated as set then items with frequencies
+    meaning we do not keep track of frequencies
+
+    if any of the ransom string does not exist in the original set
+    ransom is not replicable
+  */
+  static bool CreateRansomReplica(string[] arr, int len, Dictionary<string, int> wordDict) {
+    for (int i = 0; i < len; i++) {
+      if (wordDict.ContainsKey(arr[i]) == false || wordDict[arr[i]] == 0)
+        return false;
+      wordDict[arr[i]]--;
+    }
+    return true;
+  }
+}
---
+++ general-solving/hackerrank/Cracking_the_Coding_Interview_Challenges/Data Structures/004_balanced-brackets.cs
@@ -0,0 +1,58 @@
+﻿/***************************************************************************
+* Problem Name: Stacks: Balanced Brackets
+* Problem URL : https://www.hackerrank.com/challenges/ctci-balanced-brackets
+* Date        : Jan 28, 2016
+* Complexity  : O(n log n) Time
+* Author      : Atiq Rahman
+* Status      : Accepted
+* Notes       : 
+*               Time complexity depends on implementation of stack
+* meta        : tag-easy; tag-data-structure
+***************************************************************************/
+using System;
+using System.Collections.Generic;
+
+class Solution {
+    static bool IsLeftBracket(char ch) {
+        if (ch == '(' || ch == '{' || ch == '[')
+            return true;
+        return false;
+    }
+    
+    static char MapRightBracket(char ch) {
+        string left_br = "({[";
+        string right_br = ")}]";
+        for (int i=0; i<left_br.Length; i++)
+            if (ch == right_br[i])
+                return left_br[i];
+        return '\0';
+    }
+    
+    static bool IsBalanced(string str) {
+        Stack<char> br_stack = new Stack<char>();
+        
+        for (int i=0; i < str.Length; i++) {
+            if (IsLeftBracket(str[i]))
+                br_stack.Push(str[i]);
+            else {
+                if (br_stack.Count == 0)
+                    return false;
+                char ch = br_stack.Pop();
+                if (ch != MapRightBracket(str[i]))
+                    return false;
+            }
+        }
+        return br_stack.Count == 0;
+    }
+    
+    static void Main(String[] args) {
+        int t = Convert.ToInt32(Console.ReadLine());
+        for(int a0 = 0; a0 < t; a0++){
+            string expression = Console.ReadLine();
+            if (IsBalanced(expression))
+                Console.WriteLine("YES");
+            else
+                Console.WriteLine("NO");
+        }
+    }
+}
