731a332b16807ee4aad05d4085b17c711a93b379
Atiq Rahman <atiqcx@gmail.com>
2017-12-16 14:30:27 -0800

    codeforces/451B_SortArray.cs   Algorithm, explanation and specs are added

---
+++ online-problem-solving/codeforces/451B_SortArray.cs
@@ -0,0 +1,82 @@
+﻿/***************************************************************************
+* Title       : Sort the Array
+* URL         : http://codeforces.com/problemset/problem/451/B
+* Occasion    : Codeforces Round #258 (Div. 2)
+* Date        : Nov 1 2017
+* Complexity  : O(n) 78ms, Space O(n), 11300 KB
+* Author      : Atiq Rahman
+* Status      : Accepted
+* Notes       : Naive: sort the array and find diff sequence to reverse
+*   Approach O(n),
+*   Finding the starting and ending index of the segment that is in reverse
+*   order.
+*
+*   Input: distinct integers, not equal
+*   Judge has some large inputs, good for testing.
+*
+* meta        : tag-sorting, tag-easy
+***************************************************************************/
+using System;
+
+class RSSolution {  // Reverse segment Solution Class
+  private int n;
+  private int[] A;
+  private int start;
+  private int end;
+
+  public void TakeInput() {
+    n = int.Parse(Console.ReadLine());
+    A = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+  }
+
+  /*
+   * Returns true if a reverse interval that fits the requirements is found
+   * Sets class members: start, end
+   * 
+   * Explanation of the Algorithm:
+   * find the index from which numbers are not in sorted order anymore
+   * From that index, keep searching and find the index till which numbers are
+   * not in reverse sorted order.
+   * 
+   * Check if found segment of the array that is in reverse sorted array falls
+   * within the range of the original array
+   */
+  private bool GetReverseInterval() {
+    int i;
+    // all a[i] are distinct < or <= should work
+    for (i=0; i < n-1 && A[i] < A[i + 1]; i++);
+    start = i;
+    // a[i] are distinct
+    for (i=start; i < n-1 && A[i] > A[i + 1]; i++);
+    end = i;
+    // verify if rest of the array is sorted
+    // index end is verified by previous loop; start from end+1
+    for (i = end+1; i < n - 1 && A[i] < A[i + 1]; i++);
+    if (i < n - 1)  // not sorted
+      return false;
+    // check if found interval falls inside the array
+    // first part of the condition checks if the first element in the reverse
+    // segment is smaller than the next item which lies right after the segment
+    // Second part checks if previous element lying right before the segement
+    // is less than the last element in the segment
+    return (end+1==n || A[start] < A[end + 1]) && (start==0 || A[start-1] < A[end]);
+  }
+
+  public void Run() {
+    if (GetReverseInterval()) {
+      Console.WriteLine("yes");
+      start++; end++; // convert to 1-based index
+      Console.WriteLine(start + " " + end);
+    }
+    else
+      Console.WriteLine("no");
+  }
+}
+
+public class CF_Solution {
+  private static void Main() {
+    RSSolution revSegmentDemo = new RSSolution();
+    revSegmentDemo.TakeInput();
+    revSegmentDemo.Run();
+  }
+}
