72db46c3f900fce7fc834d70c8382deb938d2fa1
Atiq Rahman <atiqcx@gmail.com>
2017-12-17 14:54:55 -0800

    A leetcode dfs solution added

---
+++ general-solving/leetcode/515_find-largest-value-in-each-tree-row.cs
@@ -0,0 +1,33 @@
+﻿/******************************************************************************
+* Title : Find Largest Value in Each Tree Row
+* URL   : https://leetcode.com/problems/find-largest-value-in-each-tree-row
+* Date  : 2017-12-17
+* Author: Atiq Rahman
+* Comp  : O(n)
+* Status: Accepted
+* Notes : Solvable using DFS, BFS
+*   This technique is a dfs technique, uses recursion to implement
+*   depth definition:
+*     https://fftsys.azurewebsites.net/atiq/tech/binary-tree-jargon
+* meta  : tag-binary-tree, tag-dfs, tag-bfs, tag-recursion
+******************************************************************************/
+public class Solution {
+  List<int> largestValueInRows;
+  
+  public IList<int> LargestValues(TreeNode root) {
+    largestValueInRows = new List<int>();
+    GetLargestValuesLevelWise(root, 0);
+    return largestValueInRows;
+  }
+  
+  private void GetLargestValuesLevelWise(TreeNode root, int depth) {
+    if (root == null)
+      return ;
+    if (largestValueInRows.Count < depth + 1)
+      largestValueInRows.Add(root.val);
+    else
+      largestValueInRows[depth] = Math.Max(largestValueInRows[depth],root.val);
+    GetLargestValuesLevelWise(root.left, depth + 1);
+    GetLargestValuesLevelWise(root.right, depth + 1);
+  }
+}
