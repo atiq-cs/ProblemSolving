b30f0a0c15ae40406c24561f3b8a5c5f250349b2
Atiq Rahman <atiqcx@gmail.com>
2017-12-22 20:16:50 -0800

    For valid-parentheses problem, algorithm description time and space complexity added.

--- general-solving/hackerrank/CCI/DataStructure/005_balanced-brackets.cs
+++ general-solving/hackerrank/CCI/DataStructure/005_balanced-brackets.cs
@@ -5,54 +5,54 @@
 * Complexity  : O(n log n) Time
 * Author      : Atiq Rahman
 * Status      : Accepted
-* Notes       : 
-*               Time complexity depends on implementation of stack
-* meta        : tag-easy; tag-data-structure
+* Notes       : Time complexity depends on implementation of stack
+*   more desc at 'leetcode/020_valid-parentheses.cs'
+* meta        : tag-easy, tag-data-structure, tag-stack
 ***************************************************************************/
 using System;
 using System.Collections.Generic;
 
 class Solution {
-    static bool IsLeftBracket(char ch) {
-        if (ch == '(' || ch == '{' || ch == '[')
-            return true;
-        return false;
-    }
+  static bool IsLeftBracket(char ch) {
+    if (ch == '(' || ch == '{' || ch == '[')
+      return true;
+    return false;
+  }
+  
+  static char MapRightBracket(char ch) {
+    string left_br = "({[";
+    string right_br = ")}]";
+    for (int i=0; i<left_br.Length; i++)
+      if (ch == right_br[i])
+        return left_br[i];
+    return '\0';
+  }
+  
+  static bool IsBalanced(string str) {
+    Stack<char> br_stack = new Stack<char>();
     
-    static char MapRightBracket(char ch) {
-        string left_br = "({[";
-        string right_br = ")}]";
-        for (int i=0; i<left_br.Length; i++)
-            if (ch == right_br[i])
-                return left_br[i];
-        return '\0';
+    for (int i=0; i < str.Length; i++) {
+      if (IsLeftBracket(str[i]))
+        br_stack.Push(str[i]);
+      else {
+        if (br_stack.Count == 0)
+          return false;
+        char ch = br_stack.Pop();
+        if (ch != MapRightBracket(str[i]))
+          return false;
+      }
     }
-    
-    static bool IsBalanced(string str) {
-        Stack<char> br_stack = new Stack<char>();
-        
-        for (int i=0; i < str.Length; i++) {
-            if (IsLeftBracket(str[i]))
-                br_stack.Push(str[i]);
-            else {
-                if (br_stack.Count == 0)
-                    return false;
-                char ch = br_stack.Pop();
-                if (ch != MapRightBracket(str[i]))
-                    return false;
-            }
-        }
-        return br_stack.Count == 0;
+    return br_stack.Count == 0;
+  }
+  
+  static void Main(String[] args) {
+    int t = Convert.ToInt32(Console.ReadLine());
+    for(int a0 = 0; a0 < t; a0++){
+      string expression = Console.ReadLine();
+      if (IsBalanced(expression))
+        Console.WriteLine("YES");
+      else
+        Console.WriteLine("NO");
     }
-    
-    static void Main(String[] args) {
-        int t = Convert.ToInt32(Console.ReadLine());
-        for(int a0 = 0; a0 < t; a0++){
-            string expression = Console.ReadLine();
-            if (IsBalanced(expression))
-                Console.WriteLine("YES");
-            else
-                Console.WriteLine("NO");
-        }
-    }
+  }
 }
---
+++ general-solving/leetcode/020_valid-parentheses.cs
@@ -0,0 +1,35 @@
+﻿/***************************************************************************
+* Title : Valid Parentheses
+* URL   : https://leetcode.com/problems/valid-parentheses
+* Date  : 2017-12-13
+* Author: Atiq Rahman
+* Comp  : O(n^m * (length(file content) + length(file name)) )
+* Status: Accepted
+* Notes : Source,
+*   'general-solving/hackerrank/CCI/DataStructure/005_balanced-brackets.cs'
+*   Run time analysis,
+*    Push and pop operations on the Stack takes constant time, O(1).
+*    Implemented algorithm iterates over each character of the string.
+*    For each char, the algorithm checks if the character is an opening brace
+*    in constant time. If it is then the char is pushed into the stack in
+*    constant time. Otherwise, a char is popped from the Stack and compared.
+*    While doing this if the Stack is found empty or char does not match then,
+*    algorithm returns false. In best case, the algorithm returns false right
+*    after observing the first char.
+*    Therefore, for each char it takes constant time. Overall time complexity,
+*    in worst case, is, O(n)
+*    
+*   Space complexity analysis, 
+*    In worst case the stack might contain O(n) items. This worst case can be
+*    produced by a string containing all left/opening braces.
+*    
+*   Ref: msdn - Data Structures & Algorithms complexity
+*   https://code.msdn.microsoft.com/windowsapps/Data-Structures-Algorithms-d68f1418
+*   
+* meta  : tag-easy, tag-data-structure, tag-stack
+***************************************************************************/
+public class Solution {
+  public bool IsValid(string s) {
+    return IsBalanced(s);
+  }
+}
