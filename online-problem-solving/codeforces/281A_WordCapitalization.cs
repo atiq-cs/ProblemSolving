453fbf21924400a85e8a49bdf7ed65058e1bee00
Atiq Rahman <atiqcx@gmail.com>
2018-01-29 16:32:12 -0800

    easy string manipulation problem solution added

---
+++ online-problem-solving/codeforces/281A_WordCapitalization.cs
@@ -0,0 +1,25 @@
+﻿/***************************************************************************
+* Title : Word Capitalization
+* URL   : http://codeforces.com/problemset/problem/281/A
+* Contst: Codeforces Round #172 (Div. 2)
+* Date  : 2018-01-29
+* Author: Atiq Rahman
+* Comp  : O(n), if mutable string is used then O(1)
+* Status: Accepted
+* Notes : solves replacing the first character of a string
+* meta  : tag-string, tag-easy
+***************************************************************************/
+using System;
+
+class CF_Solution {
+  private static string Capitalize(string str) {
+    if (Char.IsUpper(str[0]))
+      return str;
+    return Char.ToUpper(str[0]) + str.Substring(1);
+  }
+
+  public static void Main() {
+    string str = Console.ReadLine();
+    Console.WriteLine(Capitalize(str));
+  }
+}
