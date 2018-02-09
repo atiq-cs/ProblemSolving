bae7db541075021dd8bff960c1467de2b94af4ad
Atiq Rahman <atiqcx@gmail.com>
2018-02-09 10:54:24 -0800

    Equivalent to what strstr does, find first occurrence

---
+++ general-solving/leetcode/028_implement-strstr.cs
@@ -0,0 +1,19 @@
+﻿/***************************************************************************
+* Title : Implement strStr()
+* URL   : https://leetcode.com/problems/implement-strstr
+* Date  : 2018-01-06
+* Author: Atiq Rahman
+* Comp  : O(n*m)
+* Status: Accepted
+* Notes : for each index in source string check if the substring from that
+*   index matches with the needle
+* meta  : tag-easy, tag-string
+***************************************************************************/
+public class Solution {
+  public int StrStr(string haystack, string needle) {
+    for (int i=0; i <= haystack.Length - needle.Length; i++)
+      if (haystack.Substring(i, needle.Length) == needle)
+        return i;
+    return -1;
+  }
+}
