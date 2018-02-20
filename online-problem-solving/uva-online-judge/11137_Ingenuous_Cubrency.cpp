ec027eb67e8b2999bc6ec88b70c451263801515c
Atiq Rahman <atiqcx@gmail.com>
2018-02-19 22:09:06 -0800

    A coin change problem solved using C++

---
+++ online-problem-solving/uva-online-judge/11137_Ingenuous_Cubrency.cpp
@@ -0,0 +1,49 @@
+/***************************************************************************
+* Title : Ingenuous Cubrency
+* URL   : https://uva.onlinejudge.org/external/111/11137.pdf
+* Date  : 2018-02-19
+* Author: Atiq Rahman
+* Comp  : O(N * M)
+* Status: Accepted
+* Notes : Classic coin change DP
+* meta  : tag-coin-change, tag-dynamic-programming
+***************************************************************************/
+#include <iostream>
+#include <vector>
+
+class DP {
+private:
+  const int LIMIT;
+  std::vector<long long> numWays;
+  std::vector<int> coins;
+
+public:
+  DP() : LIMIT(9999), numWays(LIMIT + 1, 0) {}
+
+  void Precompute() {
+    numWays[0] = 1;
+
+    for (int j = 1; j*j*j < LIMIT; j++)
+      coins.push_back(j*j*j);
+
+    for (int i = 0; i < (int)coins.size(); i++)
+      for (int j = coins[i]; j <= LIMIT; j++)
+        numWays[j] += numWays[j - coins[i]];
+  }
+
+  long long GetNumWays(int i) {
+    return numWays[i];
+  }
+};
+
+
+int main() {
+  int T;
+
+  DP demo;
+  demo.Precompute();
+  while (std::cin >> T)
+    std::cout << demo.GetNumWays(T) << std::endl;
+
+  return 0;
+}
