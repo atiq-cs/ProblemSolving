c95cfb409d29f817915d7a91e5d81a35c6f3f6d9
Atiq Rahman <atiqcx@gmail.com>
2018-02-19 17:59:21 -0800

    Add solution of 'Intersection of Two Linked Lists' based on Aaron's idea

--- general-solving/leetcode/160_intersection-of-two-linked-lists.cs
+++ general-solving/leetcode/160_intersection-of-two-linked-lists.cs
@@ -1,14 +1,67 @@
 ﻿/***************************************************************************
 * Title : Intersection of Two Linked Lists
 * URL   : https://leetcode.com/problems/intersection-of-two-linked-lists
-* Date  : 2018-01
 * Author: Atiq Rahman
-* Comp  : O(n), O(1) for hashset lookup
+* Comp  : O(n)
 * Status: Accepted
-* Notes : Build hashset using one linked list, lookup for each node in the
-*   other linked list. if found then that's the intersection
 * meta  : tag-easy, tag-linked-list, tag-hash-table
 ***************************************************************************/
+/* 2018-02-19
+ * Space O(1)
+ * Note: Iterate through each of nodes in both linked lists
+ *   if one of them becomes null advance the head of the other linked list so
+ *   that it reaches the node from where remaning length in the list equal to
+ *   length of the list for which iterator has become null.
+ *   
+ *   When both of them restarts iterating from equal length just check where
+ *   they match.
+ *   Based on Aaron's idea: 
+ */
+public class Solution {
+  public ListNode GetIntersectionNode(ListNode headA, ListNode headB) {
+    ListNode curA = headA;
+    ListNode curB = headB;
+
+    while (curA != curB) {
+      /* one of the iterators null so advance head and restart iterators */
+      if (curA == null) {
+        while ( curB != null ) {
+          headB = headB.next;
+          curB = curB.next;
+        }
+        curA = headA;
+        curB = headB;
+      }
+      else if (curB == null) {
+        while ( curA != null ) {
+          headA = headA.next;
+          curA = curA.next;
+        }
+        curA = headA;
+        curB = headB;
+      }
+      // keep iterating
+      else {
+        if (curA != null)
+          curA = curA.next;
+        if (curB != null)
+          curB = curB.next;
+      }
+    }
+    return curA;
+  }
+}
+
+/* My previous solution
+ * 2018-01
+ * Let,
+ *  N = Length of List A
+ *  M = Length of List B
+ * Time Complexity O(N+M)
+ * Space Complexity O(N); can be improved to O(Minimum(N,M))
+ * Note: Build hashset using one linked list, lookup for each node in the
+ *   other linked list. if found then that's the intersection
+ */
 public class Solution {
   HashSet<ListNode> BuildHash(ListNode head) {
     HashSet<ListNode> hashListA = new HashSet<ListNode>();
