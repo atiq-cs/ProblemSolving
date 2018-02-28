40907033de1c2f05a9022d3dcf6ff526edb32c35
Atiq Rahman <atiqcx@gmail.com>
2018-02-27 21:30:53 -0800

    util updated Minor: decoration, documentation updated

--- general-solving/leetcode/utils.cs
+++ general-solving/leetcode/utils.cs
@@ -3,17 +3,18 @@
 * URL   : 
 * Date  : 2018-02-12
 * Author: Atiq Rahman
-* Notes : 
+* Notes : Utility function for leetcode problems with jagged array in parameters
 ***************************************************************************/
-// Utility functions where the leetcode problem provides jagged array in parameters
-private int[][] ConvertMultiDimensionalToJagged(int[,] md) {
-  n = md.GetLength(0);
-  m = md.GetLength(1);
-  int[][] A = new int[md.GetLength(0)][];
-  for (int i=0; i<n; i++) {
-    A[i] = new int[m];
-    for (int j=0; j<m; j++)
-      A[i][j] = md[i,j];
+public class LeetcodeUtils {
+  private int[][] ConvertMultiDimensionalToJagged(int[,] md) {
+    n = md.GetLength(0);
+    m = md.GetLength(1);
+    int[][] jaggedArray = new int[md.GetLength(0)][];
+    for (int i=0; i<n; i++) {
+      jaggedArray[i] = new int[m];
+      for (int j=0; j<m; j++)
+        jaggedArray[i][j] = md[i,j];
+    }
+    return jaggedArray;
   }
-  return A;
 }
