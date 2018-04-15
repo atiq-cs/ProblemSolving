ce4412dd41c57f062fc2e91ef75247c42c15083c
Atiq Rahman <atiqcx@gmail.com>
2018-04-14 22:57:09 -0700

    Minor: notes/indentation

--- general-solving/leetcode/155_min-stack.cpp
+++ general-solving/leetcode/155_min-stack.cpp
@@ -9,29 +9,41 @@
 *                the minimum element in constant time.
 * meta        : tag-data-structure, tag-stack
 ***************************************************************************/
+#include <stack>
 
 class MinStack {
-    stack<int> regular_stack;
-    stack<int> min_stack;
-    
+  stack<int> regular_stack;
+  stack<int> min_stack;
+  
 public:
-    void push(int x) {
-        regular_stack.push(x);
-        if (min_stack.empty() || x <= min_stack.top())
-            min_stack.push(x);
-    }
+  void push(int x) {
+    regular_stack.push(x);
+    if (min_stack.empty() || x <= min_stack.top())
+      min_stack.push(x);
+  }
 
-    void pop() {
-        int item = regular_stack.top(); regular_stack.pop();
-        if (min_stack.top() == item)
-            min_stack.pop();
-    }
+  void pop() {
+    int item = regular_stack.top(); regular_stack.pop();
+    if (min_stack.top() == item)
+      min_stack.pop();
+  }
 
-    int top() {
-        return regular_stack.top();
-    }
+  int top() {
+    return regular_stack.top();
+  }
 
-    int getMin() {
-        return min_stack.top();
-    }
+  int getMin() {
+    return min_stack.top();
+  }
 };
+/*
+Draft
+4 5 1 2 3 7 3 4
+4 1
+does not touch the min stack as long as the item in popped is bigger
+continues popping min item as long as the item
+
+0 1 0
+r -  0 1 0
+min- 0 0
+*/
