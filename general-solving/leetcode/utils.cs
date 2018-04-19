216cbc2c9866fb6ec37f539bc2383406ac6061f1
Atiq Rahman <atiqcx@gmail.com>
2018-04-18 20:28:04 -0700

    Util function for Converting Jagged Array To MultiDimensional Array added

--- general-solving/leetcode/utils.cs
+++ general-solving/leetcode/utils.cs
@@ -6,7 +6,8 @@
 * Notes : Utility function for leetcode problems with jagged array in parameters
 ***************************************************************************/
 public class LeetcodeUtils {
-  private int[][] ConvertMultiDimensionalToJagged(int[,] md) {
+  private int[][] ConvertMultiDimensionalToJagged(int[,] md)
+  {
     n = md.GetLength(0);  // class member
     m = md.GetLength(1);  // class member
 
@@ -17,5 +18,21 @@ public class LeetcodeUtils {
         jaggedArray[i][j] = md[i,j];
     }
     return jaggedArray;
+  }
+
+  private int[,] ConvertJaggedToMultiDimensional(int[][] jaggedArray)
+  {
+    n = jaggedArray.Length;  // class member
+    if (n == 0)
+      throw new ArgumentException();
+    m = jaggedArray[0].Length;  // class member
+    if (m == 0)
+      throw new ArgumentException();
+
+    int[,] md = new int[n,m];
+    for (int i = 0; i < n; i++)
+      for (int j = 0; j < m; j++)
+        md[i, j] = jaggedArray[i][j];
+    return md;
   }
 }
