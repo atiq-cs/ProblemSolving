5a31c618a6f0a77a4de499ba9a09e86bd5618ee6
Atiq Rahman <atiqcx@gmail.com>
2018-04-20 16:08:25 -0700

    Notes added why things are this way and indentation updated

--- general-solving/leetcode/098_validate-binary-search-tree.cs
+++ general-solving/leetcode/098_validate-binary-search-tree.cs
@@ -5,35 +5,38 @@
 * Complexity  : O(n) Time
 * Author      : Atiq Rahman
 * Status      : Accepted
-* Notes       : A clever inorder traversal and updating the previous root each time solves the problem
-*               related: 095_validate-binary-search-tree.cpp
+* Notes       : A clever inorder traversal and updating the previous root each
+*   time solves the problem
+*   related: leetcode/095_validate-binary-search-tree.cpp
 *               
-*               Why the solution is like this?
-*               http://blogs.msdn.com/b/csharpfaq/archive/2004/05/11/why-doesn-t-c-support-static-method-variables.aspx
+*   Why the solution is like this?
+*   http://blogs.msdn.com/b/csharpfaq/archive/2004/05/11/
+*   why-doesn-t-c-support-static-method-variables.aspx
 * meta        : tag-binary-tree, tag-recursion
 ***************************************************************************/
-
 public class Solution {
-    static TreeNode prev;
+  // Moving this declaration inside a method is not supported
+  static TreeNode prev;
 
-    public bool IsValidBST(TreeNode root) {
-        prev = null;
-        return IsValidBSTRec(root);
-    }
+  // This additional method could not gotten rid of since this method is called
+  // multiple times from leetcode online judge. But, prev static property is
+  // initialized single time when the class was instantiated
+  public bool IsValidBST(TreeNode root) {
+    prev = null;
+    return IsValidBSTRec(root);
+  }
 
-    public bool IsValidBSTRec(TreeNode root) {
-        if (root == null) return true;
-        
-        if (IsValidBSTRec(root.left) == false)
-            return false;
-            
-        if (prev != null && root.val <= prev.val)
-            return false;
-        
-        prev = root;
-        
-        if (IsValidBSTRec(root.right) == false)
-            return false;
-        return true;
-    }
+  public bool IsValidBSTRec(TreeNode root) {
+    if (root == null) return true;
+    
+    if (IsValidBSTRec(root.left) == false)
+      return false;
+    if (prev != null && root.val <= prev.val)
+      return false;
+    
+    prev = root;
+    if (IsValidBSTRec(root.right) == false)
+      return false;
+    return true;
+  }
 }
