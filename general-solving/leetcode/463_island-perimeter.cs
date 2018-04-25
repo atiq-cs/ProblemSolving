394ee9ec2f102ad507b9adedf684ad8ad8be496b
Atiq Rahman <atiqcx@gmail.com>
2018-04-24 22:21:41 -0700

    2 past solutions added

---
+++ general-solving/leetcode/463_island-perimeter.cs
@@ -0,0 +1,38 @@
+﻿/***************************************************************************
+* Title : Island Perimeter
+* URL   : https://leetcode.com/problems/island-perimeter
+* Date  : 2018-02
+* Author: Atiq Rahman
+* Comp  : O(N*M)
+* Status: Accepted
+* Notes : Based on Vasili's version which essentially boils down to,
+*   if element on starting index is less than item on middle index then left
+*   (w.r.t middle element) side is sorted
+*   Otherwise, right side is sorted
+*   
+*   Because one of the two sides must be sorted in a rotated sorted array
+*   
+*   This is more comprehensive than what I was developing initially,
+*   My first idea was to consider all cases where the rotation index can be (
+*   starting from 1 to N-1). This means rotation point can be in between
+*   first and middle or middle and last
+* meta  : tag-binary-search, tag-recursion
+***************************************************************************/
+public class Solution {
+  int n, m;
+  public int IslandPerimeter(int[,] jgrid) {
+    int[][] grid = ConvertMultiDimensionalToJagged(jgrid);
+    int cellCount = 0, excludeCellCount = 0;
+
+    for (int i=0; i<n; i++)
+      for (int j=0; j<m; j++)
+        if (grid[i][j] == 1) {
+          cellCount++;
+          if (i != 0 && (grid[i-1][j] == 1))
+            excludeCellCount++;
+          if (j != 0 && (grid[i][j-1] == 1))
+            excludeCellCount++;
+        }
+    return (cellCount<<2) - (excludeCellCount<<1);
+  }
+}
---
+++ general-solving/leetcode/554_brick-wall.cs
@@ -0,0 +1,30 @@
+﻿/***************************************************************************
+* Title : Brick Wall
+* URL   : https://leetcode.com/problems/brick-wall
+* Date  : 2018-03
+* Author: Atiq Rahman
+* Comp  : O(N*M)
+* Status: Accepted
+* Notes :
+* meta  : tag-hash-table, tag-leetcode-medium
+***************************************************************************/
+public class Solution {
+  public int LeastBricks(IList<IList<int>> wall) {
+    Dictionary<int, int> brickDict = new Dictionary<int, int>();
+    foreach(IList<int> Bricks in wall) {
+      int sum = 0;
+      for(int i=0; i<Bricks.Count-1; i++) {
+        sum += Bricks[i];
+        if (brickDict.ContainsKey(sum))
+          brickDict[sum]++;
+        else
+          brickDict.Add(sum, 1);
+      }
+    }
+    int max = 0;
+    foreach (KeyValuePair<int, int> Entry in brickDict)
+      if (max < Entry.Value)
+        max = Entry.Value;
+    return wall.Count - max;
+  }
+}
