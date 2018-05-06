0c707e6d209922c844b7fd5f70f91023f4f5c3b4
Atiq Rahman <atiqcx@gmail.com>
2018-05-06 16:07:44 -0700

    ref added, indentation fixed

--- general-solving/leetcode/053_maximum-subarray.cs
+++ general-solving/leetcode/053_maximum-subarray.cs
@@ -6,25 +6,23 @@
 * Author      : Atiq Rahman
 * Status      : Accepted (176ms)
 * Notes       : Largest sum in contiguous subarray
-*               fill in ...
-*               
-* meta        : tag-dynamic-programming
+*   Kadane's Linear Algorithm to find max sum
+*   
+* ref         : https://en.wikipedia.org/wiki/Maximum_subarray_problem#Kadane's
+*               _algorithm              
+* meta        : tag-dp, tag-kadane
 ***************************************************************************/
-
 public class Solution {
-    public int MaxSubArray(int[] nums) {
-        return kadane(nums);
-    }
-    
-    private int kadane(int[] A) {
-    	// initialize max sum with first element
-    	int current_sum = A[0];
-    	int max_sum = current_sum;
-    
-    	for (int i = 1; i < (int) A.Length; i++) {
-    		current_sum = Math.Max(A[i], current_sum + A[i]);
-    		max_sum = Math.Max(max_sum, current_sum);
-    	}
-    	return max_sum;
-    }
+  public int MaxSubArray(int[] A)
+  {
+  	// initialize max sum with first element
+  	int current_sum = A[0];
+  	int max_sum = current_sum;
+  
+  	for (int i = 1; i < (int) A.Length; i++) {
+  		current_sum = Math.Max(A[i], current_sum + A[i]);
+  		max_sum = Math.Max(max_sum, current_sum);
+  	}
+  	return max_sum;
+  }
 }
