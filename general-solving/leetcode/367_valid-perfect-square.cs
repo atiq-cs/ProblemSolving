4e51ce73cc7ab1fcf36726ae47b782e720c6c7b7
Atiq Rahman <atiqcx@gmail.com>
2018-05-05 21:14:27 -0700

    square number check problem solved based on sqrt(x) solution

---
+++ general-solving/leetcode/367_valid-perfect-square.cs
@@ -0,0 +1,35 @@
+﻿/***************************************************************************
+* Title : Valid Perfect Square
+* URL   : https://leetcode.com/problems/valid-perfect-square
+* Date  : 2018-05-05
+* Author: Atiq Rahman
+* Comp  : O(lg N) Time, O(lg N) space on Stack of recursion
+* Status: Accepted
+* Notes : Check if given integer number is a perfect square
+*   Based on 'leetcode/069_sqrtx.cs'
+*
+*   Condition for '(mid+1) * (mid+1) > n' check is removed which is only required
+*   if we want to find a rounded square root integer value
+* meta  : tag-binary-search, tag-leetcode-easy
+***************************************************************************/
+public class Solution {
+  private int n;
+  public bool IsPerfectSquare(int x)
+  {
+    return IsPerfectSquareRec(0, n = x);
+  }
+
+  // recursive binary search method
+  public bool Sqrt(int start, int end)
+  {
+    int mid = (end - start) / 2 + start; // overflow fix for addition
+    long mul = (long)mid * mid;  // overflow fix for multiplication
+    if (mul == n)
+      return true;
+    if (start >= end)
+      return false;
+    if (mul > n)
+      return IsPerfectSquareRec(start, mid - 1);
+    return IsPerfectSquareRec(mid + 1, end);
+  }
+}
