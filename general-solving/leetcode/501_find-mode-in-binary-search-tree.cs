0e833d894757bb29ce6cd5fa5ecc2f8e705d632c
Atiq Rahman <atiqcx@gmail.com>
2018-05-14 16:49:52 -0700

    BST Inorder most freq problem solved without using additional space but stack space

---
+++ general-solving/leetcode/501_find-mode-in-binary-search-tree.cs
@@ -0,0 +1,53 @@
+﻿/***************************************************************************
+* Title : Find Mode in Binary Search Tree
+* URL   : https://leetcode.com/problems/find-mode-in-binary-search-tree
+* Date  : 2018-05-13
+* Author: Atiq Rahman
+* Comp  : O(n), O(n) stack space
+* Status: Accepted
+* Notes : Inorder traversal gives us sorter order for a BST. Because the items
+*   are sorted we can check consecutive items for frequency and update max.
+*   max (maxFreq) contains the most frequency.
+*   
+*   During inorder traversal we maintain previous node using a class member.
+*   We appropriately handle cases when it is null.
+* meta  : tag-leetcode-easy, tag-binary-search-tree
+***************************************************************************/
+public class Solution {
+  private List<int> modes;
+  public int[] FindMode(TreeNode root) {
+    modes = new List<int>();
+    InOrder(root);
+    if (pre == null) ; else if (maxFreq == freq)
+      modes.Add(pre.val);
+    else if (maxFreq < freq)
+    {
+      maxFreq = freq;
+      modes = new List<int>(new int[] { pre.val });
+    }
+   return modes.ToArray();
+  }
+  
+  private TreeNode pre = null;
+  private int freq = 1, maxFreq=0;
+  
+  public void InOrder(TreeNode root) {
+    if (root == null)
+      return;
+    InOrder(root.left);
+    if (pre == null) ; else if (pre.val == root.val) {
+      freq++;
+    }
+    else {
+      if (maxFreq == freq)
+        modes.Add(pre.val);
+      else if (maxFreq < freq) {
+        maxFreq = freq;
+        modes = new List<int>(new int[] { pre.val });
+      }
+      freq = 1;
+    }
+    pre = root;
+    InOrder(root.right);
+  }
+}
