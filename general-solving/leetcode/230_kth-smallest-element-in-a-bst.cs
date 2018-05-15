13f11cda2a86e6ed3fb5c0764f13b07104f4c108
Atiq Rahman <atiqcx@gmail.com>
2018-05-14 17:05:08 -0700

    BST Inorder most freq problem solved without using additional space but stack space

---
+++ general-solving/leetcode/230_kth-smallest-element-in-a-bst.cs
@@ -0,0 +1,32 @@
+﻿/***************************************************************************
+* Title : Kth Smallest Element in a BST
+* URL   : https://leetcode.com/problems/kth-smallest-element-in-a-bst
+* Date  : 2018-05-13
+* Author: Atiq Rahman
+* Comp  : O(n), O(n) stack space
+* Status: Accepted
+* Notes : Inorder traversal gives us sorter order for a BST. Because the items
+*   are sorted we can check consecutive items and decrement k each time.
+*   We need to check equality if there are duplicate values kinda similar to
+*   the ref.
+*   Why is it tagged medium?
+* rel   : 'general-solving/leetcode/501_find-mode-in-binary-search-tree.cs'
+* meta  : tag-leetcode-medium, tag-binary-search-tree
+***************************************************************************/
+public class Solution {
+  private int k, val;
+  public int KthSmallest(TreeNode root, int k) {
+    this.k = k;
+    InOrder(root);
+    return val;
+  }
+  
+  private void InOrder(TreeNode root) {
+    if (root == null)
+      return;
+    InOrder(root.left);    
+    if (--k == 0)
+      val = root.val;    
+    InOrder(root.right);
+  }
+}
