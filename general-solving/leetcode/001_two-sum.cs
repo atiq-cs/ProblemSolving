923e8b22cb7786e2861e9e52a8719b58294f9dbc
Atiq Rahman <atiqcx@gmail.com>
2018-05-24 14:01:53 -0700

    First version updated and second version further optimized

--- general-solving/leetcode/001_two-sum.cs
+++ general-solving/leetcode/001_two-sum.cs
@@ -7,35 +7,49 @@
 * Status      : Accepted
 * Notes       : hashset is a better data structure for set operations as we
 *   don't require set operations we use dictionary
-*   ref: https://leetcode.com/articles/two-sum/ (last one same as mine)
+*   ref: https://leetcode.com/articles/two-sum/
 *   Probably input number are distinct in that case we can use a hashMap
 *   instead of Dictionary
 * meta        : tag-hashtable
 ***************************************************************************/
-
 public class Solution {
-    public int[] TwoSum(int[] nums, int target) {
-        Dictionary<int, int> numDict = new Dictionary<int, int>();
-        
-        for (int i=0; i<nums.Length; i++) {
-            try {
-                numDict.Add(nums[i], i+1);
-            }
-            catch (ArgumentException) { }
-        }
-        
-        for (int i=0; i<nums.Length; i++) {
-            try {
-                int j = numDict[target-nums[i]];
-                if (i+1 == j)
-                    continue;
-                if (i+1 > j)
-                    return new int[] {j, i+1};
-                return new int[] {i+1, j};
-            }
-            catch (KeyNotFoundException) { }
-        }
-
-        return new int[] {0, 0};
+  // second version after Microsoft Interview 2018-05-21, similar to last one
+  // in above article; utilizes single loop
+  public int[] TwoSum(int[] nums, int sum)
+  {
+    var numDict = new Dictionary<int, int>();
+    for (int i = 0; i < nums.Length; i++) {
+      if (numDict.ContainsKey(sum - nums[i]))
+        return new int[] { i, numDict[sum - nums[i]] };
+      // numbers in nums can be duplicate
+      if (numDict.ContainsKey(nums[i]) == false)
+        numDict.Add(nums[i], i);
     }
+    return null;
+  }
+
+  // first version
+  public int[] TwoSumv1(int[] nums, int target) {
+    Dictionary<int, int> numDict = new Dictionary<int, int>();    
+    for (int i=0; i<nums.Length; i++) {
+      try {
+        numDict.Add(nums[i], i+1);
+      }
+      catch (ArgumentException) { }
+    }
+    
+    for (int i=0; i<nums.Length; i++) {
+      try {
+        int j = numDict[target-nums[i]];
+        if (i+1 == j)
+          continue;
+        if (i+1 > j)
+          return new int[] {j, i+1};
+        return new int[] {i+1, j};
+      }
+      catch (KeyNotFoundException) { }
+    }
+
+    return null;
+  }
 }
