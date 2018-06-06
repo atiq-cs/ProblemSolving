20bcdf1b806e7c765e190fc147524df494767ea1
Atiq Rahman <atiqcx@gmail.com>
2018-06-05 17:45:33 -0700

    input is in string, otherwise same problem

---
+++ general-solving/hackerrank/contests/002_bingacm-apr15_longest-common-subsequence.cs
@@ -0,0 +1,33 @@
+﻿/***************************************************************************
+* Title : Longest Common Subsequence
+* Contst: Binghamton ACM April 2015
+* URL   : https://www.hackerrank.com/contests/bingacm-apr15/challenges
+* Date  : 2018-06-05
+* Author: Atiq Rahman
+* Comp  : O(n)
+* Status: Accepted
+* Notes : method call arguments are in reverse order
+*
+*    Input is two strings
+* meta  : tag-lcs, tag-dp
+***************************************************************************/
+using System;
+
+// class 'SubseqAlgoDemo' is declared and defined in 'demos/algo/dp/lcs.cs'
+
+class HK_Solution {
+  // Too simple to consider use of IO template
+  static int[] Run(int[] a, int[] b) {
+    var demo = new SubseqAlgoDemo<char>();
+    var a = Console.ReadLine().ToCharArray();
+    var b = Console.ReadLine().ToCharArray();
+    Console.WriteLine(new string(demo.LongestCommonSubsequence(b, a)));
+  }
+}
+
+/*
+Considered input,
+backtrack
+algorithm
+requires ar to be answer instead of at
+*/
