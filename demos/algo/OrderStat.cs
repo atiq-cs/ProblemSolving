8630d01423141f6d7aaa82cf924600a53e6a4554
Atiq Rahman <atiqcx@gmail.com>
2018-06-12 18:06:01 -0700

    Shorter code for Partition, validation of indices added dremio version's solution added

--- demos/algo/OrderStat.cs
+++ demos/algo/OrderStat.cs
@@ -3,24 +3,34 @@
 * URL   : 
 * Date  : 2018-06
 * Author: Atiq Rahman
-* Notes : Tested with leetcode
+* Notes : RandomizedSelet modifies input array and put all items <= pivot to
+*   left side. Therefore this method can also be used to return k largest items
 ***************************************************************************/
 public class OrderStat {
   private int[] A;
   bool shouldGetIndex;
 
+  private bool AreIndicesValid(int p, int r) {
+    if (p >= r)
+      return false;
+    return p>=0;
+  }
+
   // can return index instead of the item based on shouldGetIndex
-  private int RandomizedSelet(int p, int r, int i) {
+  // preceded by AreIndicesValid() call
+  private int RandomizedSelet(int p, int r, int k) {
     if (p == r)
       return shouldGetIndex ? p : A[p];
+    // Index of q in original input array
     int q = RandomizedPartition(p, r);
-    int k_i = q - p + 1;
-    if (i == k_i)
+    // relative rank of q based on current array
+    int _q = q - p + 1;
+    if (k == _q)
       return shouldGetIndex ? q : A[q];
-    else if (i < k_i)
-      return RandomizedSelet(p, q-1, i);
+    else if (k < _q)
+      return RandomizedSelet(p, q-1, k);
     else
-      return RandomizedSelet(q + 1, r, i - k_i);
+      return RandomizedSelet(q+1, r, k-_q);
   }
 
   private int RandomizedPartition(int p, int r) {
@@ -34,16 +44,11 @@ public class OrderStat {
   private int Partition(int p, int r) {
     int i = p-1;
     long x = A[r];
-    for (int j=p; j<r; j++) {
-      /*
-       * maintains invariant that all items are less than pivot are in the
-       * block of i (or smaller elements till where i ends)
-       */
-      if (A[j] <= x) {
-        i++;
-        Swap(i, j);
-      }
-    }
+    for (int j=p; j<r; j++)
+      // maintains invariant that all items are less than pivot are in the
+      // block of i (or smaller elements till where i ends)
+      if (A[j] <= x)
+        Swap(++i, j);
     Swap(i+1, r);
     return i + 1;
   }
--- general-solving/leetcode/0215_kth-largest-element-in-an-array.cs
+++ general-solving/leetcode/0215_kth-largest-element-in-an-array.cs
@@ -6,7 +6,11 @@
 * Comp  : O(N), O(1)
 * Status: Accepted
 * Notes : tested with both values of 'shouldGetIndex'
-* meta  : tag-order-stats
+*   dremio asks a variation of this: return k largest elements
+*   Find the smallest number in window of first k items and replace with a
+*   number from the number outside of this window.
+*   ref: https://stackoverflow.com/q/14450024
+* meta  : tag-order-stats, tag-company-dremio
 ***************************************************************************/
 public class Solution {
   // RandomizedSelet is at 'demos/algo/OrderStat.cs'
@@ -14,6 +18,39 @@ public class Solution {
   {
     A = nums;
     shouldGetIndex = false;
-    return RandomizedSelet(0, A.Length-1, A.Length-k+1);
+    return AreIndicesValid(0, A.Length - 1) ? RandomizedSelet(0, A.Length-1,
+      A.Length-k+1) : -1;
   }
+
+  public static int[] kLargest_Dremio(int[] A, int k) {
+    int[] result = new int[k];
+    shouldGetIndex = true;
+    int q = AreIndicesValid(0, A.Length - 1) ? RandomizedSelet(0, A.Length-1,
+      A.Length-k+1) : -1;
+    Array.Copy(A, q, result, 0, k);
+    return result;
+  } 
+
+  // see note above, ref: https://codereview.stackexchange.com/q/98010
+  public static int[] kLargest_Dremio(int[] A, int k) {
+    int minIndex = 0, i;              //Find Min
+    for (i = k; i < A.Length; i++) {
+      minIndex = 0;
+      // O(k)
+      for (int j = 0; j < k; j++)
+        if(A[j] < A[minIndex])
+          minIndex = j;
+      if (A[minIndex] < A[i])
+        Swap(A, minIndex, i);
+    }
+    // result is q..k, copy example is in method above
+  } 
 }
+
+/*
+Experiment: do we get first k largest with RandomizedSelet?
+[3,2,3,1,2,4,5,5,6]
+4
+returns
+1 2 2 3 3
+*/
