be672be3f529d61802ad1253f068db6c3730d8c0
Atiq Rahman <atiqcx@gmail.com>
2018-06-19 11:12:37 -0700

    lamba exp in cpp, C# not available for this problem

---
+++ general-solving/hackerrank/CCI/algo/02_comparator-sorting.cpp
@@ -0,0 +1,25 @@
+﻿/***************************************************************************
+* Title : Sorting: Comparator
+* URL   : https://www.hackerrank.com/challenges/ctci-comparator-sorting
+* Date  : 2018-06
+* Author: Atiq Rahman
+* Comp  : O(n lg n) depends on partition size, ref:
+*   https://msdn.microsoft.com/en-us/library/6tf1f0bc.aspx
+* Status: Accepted
+* Notes : Lang C# not available in this problem
+* meta  : tag-sorting, tag-lambda-exp
+***************************************************************************/
+struct Player {
+  string name;
+  int score;
+};
+
+vector<Player> comparator(vector<Player> players) {
+  sort(players.begin(), players.end(),
+    [](const Player & a, const Player & b) -> bool {
+    if (a.score == b.score)
+      return a.name < b.name;
+    return a.score > b.score;
+  });
+  return players;
+}
