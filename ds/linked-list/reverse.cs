b74cf2b062875f0fb498416e946c0d1d322cbf8a
Atiq Rahman <atiqcx@gmail.com>
2018-06-20 16:41:20 -0700

    linked list reverse consolidated

---
+++ ds/linked-list/reverse.cs
@@ -0,0 +1,108 @@
+﻿/***************************************************************************
+* Title : Reverse a Linked List
+* URL   : https://leetcode.com/problems/two-sum-iv-input-is-a-bst
+* Date  : 2018-06
+* Author: Atiq Rahman
+* Comp  : O(N), O(1)
+* Status: Accepted
+* Notes : These problem has easy testcases I guess.
+*   This solution will hit null pointer exception: NullReferenceException
+*   when an empty or null tree is passed, i.e, []
+*   [1] will work because left, right both pointing at that node and value at
+*   left node won't be less than value at right node.
+* Rel   : 'leetcode/167_two-sum-ii-input-array-is-sorted.cs'
+* meta  : tag-leetcode-easy, tag-two-pointers, tag-binary-tree, tag-successor,
+*   tag-predecessor
+***************************************************************************/
+public class Solution {
+  // BST Iterator is at 'demos/ds/binary-tree/Iterator.cs'
+  // this method needs to return node instead of value for this problem
+  public TreeNode Next() {
+    return currentNode;
+  }
+
+  public bool FindTarget(TreeNode root, int k) {
+    BSTIterator nextIterator = new BSTIterator(root),
+      preIterator = new BSTIterator(root, true);
+    
+    for (TreeNode left = nextIterator.HasNext() ? nextIterator.Next() : null,
+      right = preIterator.HasNext()? preIterator.Next() : null; left.val <
+      right.val; ) {
+      int sum = left.val + right.val;
+      if (sum == k)
+        return true;
+      if (sum < k)
+        left = nextIterator.HasNext()? nextIterator.Next() : null;
+      else
+        right = preIterator.HasNext()? preIterator.Next() : null;
+    }
+    return false;
+  }
+}
+
+// recursive
+public class Solution {
+  ListNode gHead=null; 		// global head, class member
+  public ListNode ReverseList(ListNode head)
+  {
+    RecReverse(head);
+    if (head != null)
+      head.next = null;
+    return gHead;
+  }
+
+  public void RecReverse(ListNode head)
+  {
+    if (head == null) return ;
+    if (head.next == null) { gHead = head; return ; }
+    RecReverse(head.next);
+    // Reverse link safely. We can perform such change on link here because at
+    // this point, all of the nodes till this one is already in stack. We won't
+    // lose our previous nodes we change next pointer, we will still retrieve
+    // previous nodes when function returns to caller
+    head.next.next = head;
+  }
+
+  // first version: iterative
+  ListNode ReverseList(ListNode head) {
+    ListNode current = head;
+    ListNode prev = null;
+
+    while (current != null) {
+      // save current's next because we are gonna move this pointer/refreence forward
+      ListNode temp = current.next;
+      // reverse current's next pointer
+      current.next = prev;
+      // move forward previous pointer
+      prev = current;
+      // move forward current's next pointer
+      current.next = prev;  
+    }
+	  return prev;
+  }
+}
+
+/* Consider following example for this recursive solution,
+ 3 -> 4 -> 5
+
+previous node variable = null;
+
+During first iteration,
+head = 3
+ current = 3
+ head = 4 (next of 3)
+ 3 's next set to null
+ previous = 3
+
+on second iteration,
+ current = 4
+ head = 5
+ 4's next set to 3
+ previous = 4
+
+3rd iteration gives,
+ current = 5
+ head = null
+ current's next set to 4
+ previous = 5
+*/
---
+++ general-solving/leetcode/206_reverse-linked-list.cs
@@ -0,0 +1,55 @@
+﻿/***************************************************************************
+* Title : Reverse a Linked List (recurse and iteration)
+* URL   : https://leetcode.com/problems/two-sum-iv-input-is-a-bst
+* Date  : 2015-07-29
+* Author: Atiq Rahman
+* Comp  : O(N); O(1), O(N) in stack
+* Status: Accepted
+* Notes : We discuss here both of recursive and iterative solutions
+*   Concept: Recursive
+*   uses the idea that all the nodes of the linked list are pushed into heap
+*   during recursive calls. So when we are return from those calls we utilize
+*   store nodes to change the link each time. This changes linked list's head's
+*   next as well. So we cannot find the new Head which is the tail of original
+*   list. To keep a copy of that tail we use a global/class member variable
+*   head.
+*   During recursion, we take care of the case when head.next is null.
+*   if head is null we don't need to reverse anything.
+*   
+*   All classes are assigned and passed to functions by reference whereas
+*   structures instantiate a new object and copies the content
+*   
+*   Code updated on occassion Den meetup 2018-04-21
+*   Based on 'ds/linked-list/reverse-linked-list-demo.cpp'
+*   
+*   Concept: iterative
+*   Iteratively traverse the linked list and keeps reversing the next pointers
+*   consider when changing the next pointer the next chain is lost, so use a
+*   temporary variable to keep previous node before losing chain.
+*   
+*   Technical - Understanding of reference assignment, ref:
+*   http://stackoverflow.com/q/1219664
+*   This, however, is not possible for strings(immutable)
+*   http://stackoverflow.com/q/9792776
+*
+The note below I put when I was learning C#. Helps to understand how class
+objects/references are managed in C#. However, C++ class objects are real
+objects, copy by value is predominant.
+
+A class is a reference type. When an object of the class is created, the
+variable to which the object is assigned holds only a reference to that memory.
+When the object reference is assigned to a new variable, the new variable
+refers to the original object. Changes made through one variable are reflected
+in the other variable because they both refer to the same data.
+
+A struct is a value type. When a struct is created, the variable to which the
+struct is assigned holds the struct's actual data. When the struct is assigned
+to a new variable, it is copied. The new variable and the original variable
+therefore contain two separate copies of the same data. Changes made to one
+copy do not affect the other copy. (similar to C++ classes/structs)
+
+draft note is at bottom and Code at 'ds/linked-list/reverse.cs'
+ref: https://msdn.microsoft.com/en-us/library/vstudio/ms173109.aspx
+*   Code at 'ds/linked-list/reverse.cs'
+* meta  : tag-linked-list, tag-recursion
+***************************************************************************/
