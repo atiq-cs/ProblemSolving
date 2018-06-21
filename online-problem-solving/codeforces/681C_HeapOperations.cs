bc822c358c124fa6c8a9143adf09f60926904900
Atiq Rahman <atiqcx@gmail.com>
2018-06-20 17:28:42 -0700

    tag/refactor/minor note

--- online-problem-solving/codeforces/681C_HeapOperations.cs
+++ online-problem-solving/codeforces/681C_HeapOperations.cs
@@ -14,92 +14,26 @@
 *               in the record, and the heap is non-empty when getMin ad
 *                removeMin are applied
 *
-*              MinHeap implementation is based on,
-*               hackerrank\CCI\DataStructure\008_find-the-running-median.cs
-*              For exceptions look at,
+*              msdn exceptions ref:
 *               https://docs.microsoft.com/en-us/dotnet/csharp/
 *               programming-guide/exceptions/creating-and-throwing-exceptions
-* meta        : tag-heap, tag-easy
+* meta        : tag-heap, tag-data-structure
 ***************************************************************************/
 using System;
 using System.Collections.Generic;
 
-class MinHeap {
-  protected List<int> Arr;
-  protected int Size;
-
-  public MinHeap() {
-    Arr = new List<int>();
-    Size = 0;
-  }
-
-  protected int GetParent(int i) { return (i - 1) / 2; }
-  protected int GetLeftChild(int i) { return 2 * i + 1; }
-  protected int GetRightChild(int i) { return 2 * i + 2; }
-
-  public int HeapSize() { return Size; }
-
-  public int Peek() {
-    if (Size == 0)
-      throw new InvalidOperationException("MinHeap is empty!");
-    return Arr[0];
-  }
-
-  protected void Swap(int i, int j) {
-    int tmp = Arr[i];
-    Arr[i] = Arr[j];
-    Arr[j] = tmp;
-  }
-
-  public void Insert(int item) {
-    if (Size < Arr.Count)
-      Arr[Size] = item;
-    else
-      Arr.Add(item);
-    int i = Size++;
-    // Similar to Decrease Key
-    while (i > 0 && Arr[GetParent(i)] > Arr[i]) {
-      Swap(i, GetParent(i));
-      i = GetParent(i);
-    }
-  }
-
-  protected void Heapify(int i) {
-    int l = GetLeftChild(i);
-    int r = GetRightChild(i);
-    int smallest = i;
-    if (l < Size && Arr[l] < Arr[smallest])
-      smallest = l;
-    if (r< Size && Arr[r] < Arr[smallest])
-      smallest = r;
-    if (smallest != i) {
-      Swap(smallest, i);
-      Heapify(smallest);
-    }
-  }
-
-  public int ExtractMin() {
-    if (Size == 0)
-      throw new InvalidOperationException("MinHeap underflow!");
-    int max = Arr[0];
-    Arr[0] = Arr[--Size];
-    Heapify(0);
-    return max;
-  }
-}
-
 /*
- * Advantage of maintaining HeapOpList
- * In C# usually chunk output is faster
+ * Heap is at 'ds/Heap.cs'
  * 
+ * Chunk output is faster, therefore the advantage of maintaining HeapOpList
  */
 class HeapDemo {
   private List<string> HeapOpList;
-  MinHeap min_heap;
+  Heap minHp;
 
   public void TakeInput() {
     int T = int.Parse(Console.ReadLine());
-    min_heap = new MinHeap();
+    minHp = new Heap((a, b) => { return a < b; });
     HeapOpList = new List<string>();
 
     while (T-- > 0) {
@@ -107,7 +41,7 @@ class HeapDemo {
       string[] tokens = line.Split();
       switch (tokens[0]) {
         case "insert":  // insert
-          min_heap.Insert(int.Parse(tokens[1]));
+          minHp.Insert(int.Parse(tokens[1]));
           HeapOpList.Add(line);
           break;
         case "removeMin":  // remove minimum
@@ -116,10 +50,10 @@ class HeapDemo {
            * Goal: make the heap non-empty
            * non-empty heap makes online-judge happy
            */
-          if (min_heap.HeapSize() == 0)
+          if (minHp.HeapSize() == 0)
             HeapOpList.Add("insert 1");
           else
-            min_heap.ExtractMin();
+            minHp.ExtractMin();
           HeapOpList.Add(line);
           break;
         case "getMin":  // print minimum
@@ -139,14 +73,14 @@ class HeapDemo {
    */
   private void AdjustHeapOps(int expectedMin) {
     // remove min till it becomes >= to expected
-    while (min_heap.HeapSize() > 0 && min_heap.Peek() < expectedMin) {
-      min_heap.ExtractMin();
+    while (minHp.HeapSize() > 0 && minHp.Peek() < expectedMin) {
+      minHp.ExtractMin();
       HeapOpList.Add("removeMin");
     }
     // Greater or equal to here
     // for top item greater than expected we insert
-    if (min_heap.HeapSize() == 0 || min_heap.Peek() > expectedMin) {
-      min_heap.Insert(expectedMin);
+    if (minHp.HeapSize() == 0 || minHp.Peek() > expectedMin) {
+      minHp.Insert(expectedMin);
       HeapOpList.Add("insert " + expectedMin);
     }
     // for the case where top item = expectedMin; we do nothing
@@ -161,15 +95,14 @@ class HeapDemo {
 
 public class CF_Solution {
   private static void Main() {
-    HeapDemo heap_demo = new HeapDemo();
-    heap_demo.TakeInput();
-    heap_demo.OutputHeapOpsList();
+    HeapDemo demo = new HeapDemo();
+    demo.TakeInput();
+    demo.OutputHeapOpsList();
   }
 }
 
 /*
- Sample input,
- 1
- removeMin
-
+Sample input,
+1
+removeMin
 */
