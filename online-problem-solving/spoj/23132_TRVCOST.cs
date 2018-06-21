9fdbd3aceb5d0f59b0c1c1f448fc715613b9d197
Atiq Rahman <atiqcx@gmail.com>
2018-06-21 15:31:28 -0700

    class PriorityQueue derived from Heap moved to 'ds/PriorityQueue.cs'

--- online-problem-solving/spoj/23132_TRVCOST.cs
+++ online-problem-solving/spoj/23132_TRVCOST.cs
@@ -34,83 +34,19 @@ using System.Collections.Generic;
  * Due to generic class implementation of 'PriorityQueue' it cannot access
  * members of the passed type. To reach an agreement with the class what
  * members must exist inside the type use following interface to inherit
- * specify should exist members inside the interface
- */
+ * specify should exist members inside the interface.
+ * 
+ * Access to index is not required to the 'PriorityQueue' class. so removed,
+ *  int GetIndex();
+ *  
+ * GetValue() provides the priority for the priority queue items
+ * enables access to distance member to 'PriorityQueue' class so that the
+ * class can do comparison based on distance and keep the heap arranged
+ * accordingly
+ *
 interface PriorityQueueElement {
-  // Access to index is not required to the 'PriorityQueue' class
-  // int GetIndex();
-  /* 
-   * enables access to distance member to 'PriorityQueue' class so that the
-   * class can do comparison based on distance and keep the heap arranged
-   * accordingly
-   */
   uint GetValue();
-}
-
-class PriorityQueue <T> where T:PriorityQueueElement {
-  protected List<T> Arr;
-  protected int Size;
-
-  public PriorityQueue() {
-    Arr = new List<T>();
-    Size = 0;
-  }
-
-  protected int GetParent(int i) { return (i - 1) / 2; }
-  protected int GetLeftChild(int i) { return 2 * i + 1; }
-  protected int GetRightChild(int i) { return 2 * i + 2; }
-
-  // getter only property
-  public int Count { get { return Size; } }
-
-  public T Peek() {
-    if (Size == 0)
-      throw new InvalidOperationException("MinHeap is empty!");
-    return Arr[0];
-  }
-
-  protected void Swap(int i, int j) {
-    T tmp = Arr[i];
-    Arr[i] = Arr[j];
-    Arr[j] = tmp;
-  }
-
-  public void Enqueue(T item) {
-    if (Size < Arr.Count)
-      Arr[Size] = item;
-    else
-      Arr.Add(item);
-    int i = Size++;
-    // Similar to Decrease Key
-    while (i > 0 && Arr[GetParent(i)].GetValue() > Arr[i].GetValue()) {
-      Swap(i, GetParent(i));
-      i = GetParent(i);
-    }
-  }
-
-  protected void Heapify(int i) {
-    int l = GetLeftChild(i);
-    int r = GetRightChild(i);
-    int smallest = i;
-    if (l < Size && Arr[l].GetValue() < Arr[smallest].GetValue())
-      smallest = l;
-    if (r< Size && Arr[r].GetValue() < Arr[smallest].GetValue())
-      smallest = r;
-    if (smallest != i) {
-      Swap(smallest, i);
-      Heapify(smallest);
-    }
-  }
-
-  public T Dequeue() {
-    if (Size == 0)
-      throw new InvalidOperationException("MinHeap underflow!");
-    T max = Arr[0];
-    Arr[0] = Arr[--Size];
-    Heapify(0);
-    return max;
-  }
-}
+}*/
 
 public class Edge {
   public int u;
@@ -119,14 +55,14 @@ public class Edge {
   public Edge(int u, int v, uint c) { this.u = u; this.v = v; this.c = c; }
 }
 
-public class Vertex : PriorityQueueElement {
-  public int i;
+public class Vertex {
+  // don't use i for space efficiency
+  // public int i;
   public uint d;
   // public int p; // parent member is not necessary for dijkstra
 
-  public Vertex(int i, uint d) { this.i = i; this.d = d; }
-  // public int GetIndex() { return i; }
-  public uint GetValue() { return d; }
+  public Vertex(/*int i,*/ uint d) { /*this.i = i;*/ this.d = d; }
+  // public uint GetValue() { return d; }
 }
 
 
@@ -171,7 +107,7 @@ public class Dijkstra {
 
     // Initialize for Single Source Shortest Path algo
     for (int i = 0; i < nV; i++) {
-      Vertices[i] = new Vertex(i, INF);
+      Vertices[i] = new Vertex(INF);
       // AdjList[i] = new List<int>();
       AdjMatrix[i] = new uint[nV];
       // initializing cost in adjacency matrix is not required
@@ -193,21 +129,20 @@ public class Dijkstra {
   }
 
   public void Run() {
-    PriorityQueue<Vertex> queue = new PriorityQueue<Vertex>();
-    queue.Enqueue(Vertices[Source]);
+    PriorityQueue queue = new PriorityQueue(Vertices);
+    queue.Enqueue(Source);
 
     while (queue.Count > 0) {
-      Vertex u = queue.Dequeue();
+      int u = queue.Dequeue();
 
-      if (u.d != INF) {
-        for (int i = 0; i < nV; i++) {
-          Vertex v = Vertices[i];
-          uint w = AdjMatrix[u.i][v.i];
+      if (Vertices[u].d != INF) {
+        for (int v = 0; v < nV; v++) {
+          uint w = AdjMatrix[u][v];
           // adjacent means entry on adj mat != 0; 0 because of initialization
           if (u != v && w != 0) {
             // relax all v using u
-            if (v.d > u.d + w) {
-              v.d = u.d + w;
+            if (Vertices[v].d > Vertices[u].d + w) {
+              Vertices[v].d = Vertices[u].d + w;
               // replace with decrease key
               queue.Enqueue(v);
             }
