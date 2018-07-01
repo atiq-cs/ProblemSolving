75eb5400da5ac0d0fa9c9e67e941d82ba9d82765
Atiq Rahman <atiqcx@gmail.com>
2018-06-30 17:42:58 -0700

    KMP String Matcher algo generic solution added Added note for the leetcode problem

---
+++ algo/string/KMP-String-Matcher.cs
@@ -0,0 +1,83 @@
+/***************************************************************************
+* Title : String Matching Algorithm - Knuth–Morris–Pratt
+* Date  : 2018-06-30 (update)
+* Author: Atiq Rahman
+* Comp  : O(m+n)
+* Status: Demo
+* Notes : Build Prefix Table and use the prefix table to find length of the
+*   common substring between needle and haystack string.
+* 
+*   Each index of the prefix table keeps track of the previous index from
+*   where it should start matching again when a mismatch is found.
+*   
+*   Explanation of 'ComputePrefix' method,
+*   In summary, steps for each char during iteration,
+*   1. Did it match?
+*   Revert k back to a previous index.
+*   2. Did it match now?
+*   Move forward k to next index.
+*   3. Set previous of current char's index to where k pointing now
+*   
+*   Please note that, till a char is matched with first char of the string k
+*   is < 0 and first step is never executed. After the first match second step
+*   is executed and k is moved forward.
+*   
+*   For example,
+*    we consider the pattern string: "abcabcac"
+*     
+*   Previous of first index would be,
+*     prefix[0] = -1,            'a', k = -1    
+*     prefix[1] = -1,            'b'
+*     prefix[2] = -1,            'c'
+*     prefix[3] =  0,            'a', k = 0
+*    This is the first time a match is found with first index; k moves forward.
+*    First step, now comes into effect. Because it matches now it goes to
+*    second step which moves k forward.
+*     prefix[4] = 1,            'b'
+*     prefix[5] = 2,            'c'
+*     prefix[6] = 3,            'a', k = 3
+*    i=7, now 4 and 7 does not match. k reverts back to pf[3] = 0 (points to 'a')
+*    still 1 and 7 does not match. k doesn't move forward.
+*     prefix[7] = 0
+*   
+*   'KMPStringMatcher' works similarly. However it does not modify prefix
+*   table.
+*
+*   KMP on youtube: https://www.youtube.com/watch?v=5i7oKodCRJo 
+*   Related: http://a2oj.com/Category.jsp?ID=29
+*   ref: Introduction to Algorithms, C.L.R.S Chapter 32, p1005
+* Rel   : https://www.hackerrank.com/challenges/string-similarity
+* meta  : tag-string, tag-kmp
+***************************************************************************/
+public class StringMatcherUtil {
+  private int[] ComputePrefix(string needle) {
+    int[] pf = new int[needle.Length];
+
+    pf[0] = -1; // first index is initialized
+    for (int i = 1, k=-1; i < needle.Length; i++) {
+      while (k >= 0 && needle[k + 1] != needle[i])
+        k = pf[k];
+      if (needle[k + 1] == needle[i])
+        k++;
+      pf[i] = k;
+    }
+    return pf;
+  }
+
+  public List<int> KMPStringMatcher(string haystack, string needle) {
+    var matches = new List<int>();
+    if (string.IsNullOrEmpty(needle)) return 0;
+    int[] pf = ComputePrefix(needle);
+    for (int i=0, q=-1; i<haystack.Length; i++) {
+      while (q >= 0 && needle[q + 1] != haystack[i])
+        q = pf[q];
+      if (needle[q + 1] == haystack[i])
+        q++;
+      if (q == needle.Length-1) {
+        matches.Add(i-q);
+        q = pf[q];
+      }
+    }
+    return matches;
+  }
+}
--- general-solving/leetcode/028_implement-strstr.cs
+++ general-solving/leetcode/028_implement-strstr.cs
@@ -3,13 +3,22 @@
 * URL   : https://leetcode.com/problems/implement-strstr
 * Date  : 2018-01-06
 * Author: Atiq Rahman
-* Comp  : O(n*m)
+* Comp  : O(n+m)
 * Status: Accepted
-* Notes : for each index in source string check if the substring from that
-*   index matches with the needle
-* meta  : tag-easy, tag-string
+* Notes : 
+* meta  : tag-string, tag-kmp
 ***************************************************************************/
 public class Solution {
+  // KMP is at 'algo/string/KMP-String-Matcher.cs' 
+  // method signature should be changed and code block inside to return single
+  // index instead of a list of indices
+  public int StrStr(string haystack, string needle) {
+    if (q == needle.Length - 1)
+      return i-q;   // single index of match
+    // after the loop
+    return -1;  // when no match is found
+  }
+  // Naive String Matcher C.L.R.S, p988, O(n*m)
   public int StrStr(string haystack, string needle) {
     for (int i=0; i <= haystack.Length - needle.Length; i++)
       if (haystack.Substring(i, needle.Length) == needle)
