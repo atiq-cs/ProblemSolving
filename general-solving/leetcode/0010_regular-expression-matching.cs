5e7e0b1b2f402d62f037a90b49c1a15d38877bdf
Atiq Rahman <atiqcx@gmail.com>
2018-07-10 13:39:48 -0700

    regex matching dp problem solved

---
+++ general-solving/leetcode/0010_regular-expression-matching.cs
@@ -0,0 +1,56 @@
+﻿/***************************************************************************
+* Title : Regular Expression Matching
+* URL   : https://leetcode.com/problems/regular-expression-matching
+* Date  : 2018-07-10
+* Author: Atiq Rahman
+* Comp  : O(nm), O(nm)
+* Status: Accepted
+* Notes : Implement regex matching supporting '.' and '*'
+* 
+*   Index mapping: dp[1][1] maps to index of str at 0 and index of pattern at 0
+*   youtube video adds X in the beginning of string and pattern which is not
+*   required if initialization is done properly for *
+*   ref video: https://www.youtube.com/watch?v=l3hda49XcDE
+*   Example in the video: a*b.c and aabyc
+*   
+*   ref for initialization part: https://leetcode.com/problems/regular-
+*    expression-matching/discuss/5651/Easy-DP-Java-Solution-with-detailed-
+*    Explanation
+*    
+*   https://leetcode.com/articles/regular-expression-matching/
+* Rel   : https://leetcode.com/problems/wildcard-matching
+* meta  : tag-leetcode-hard, tag-string, tag-dp, tag-backtracking, tag-microsoft
+***************************************************************************/
+public class Solution {
+  public bool IsMatch(string s, string p) {
+    // Memory Allocation
+    bool[][] dp = new bool[p.Length + 1][];
+    for (int i=0; i<=p.Length; i++)
+      dp[i] = new bool[s.Length+1];
+
+    // Initialization
+    // Compiler initialized rest of the array
+    // set true for patterns like a*b*
+    for (int i=1; i<=p.Length; i++)
+      if (p[i-1] == '*' && dp[i-2][0])
+        dp[i][0] = true;
+    
+    for (int i=1; i<=p.Length; i++)
+      for (int j=1; j<=s.Length; j++) {
+        if (p[i-1] == '*') {
+          if (i>1 && dp[i-2][j])
+            dp[i][j] = dp[i-2][j];
+          else if (s[j-1] == p[i-2] || p[i-2] == '.')
+            dp[i][j] = dp[i][j-1];
+        }
+        else if (p[i-1] == s[j-1] || p[i-1] == '.')
+          dp[i][j] = dp[i-1][j-1];
+      }
+      return dp[p.Length][s.Length];
+  }
+}
+/*
+when not intialize for * this will fail,
+"aa"
+"a*"
+*/
