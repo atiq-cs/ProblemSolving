12b5da16e9ce4692c2c959aae74840d66898f03b
Atiq Rahman <atiqcx@gmail.com>
2018-07-11 15:35:50 -0700

    an easy problem solved

---
+++ general-solving/leetcode/0783_minimum-distance-between-bst-nodes.cs
@@ -0,0 +1,27 @@
+﻿/***************************************************************************
+* Title : Minimum Distance Between BST Nodes
+* URL   : https://leetcode.com/problems/minimum-distance-between-bst-nodes
+* Date  : 2018-07 (first met in Inno World Meetup a few weeks back)
+* Author: Atiq Rahman
+* Comp  : O(N), O(1)
+* Status: Accepted
+* Notes :
+* Rel   : 'leetcode/0098_validate-binary-search-tree.cs'
+* meta  : tag-leetcode-easy, tag-binary-tree, tag-recursion, tag-inorder
+***************************************************************************/
+public class Solution {
+  private int minDiff = int.MaxValue;
+  TreeNode prev = null;
+  
+  public int MinDiffInBST(TreeNode root) {
+    if (root == null) return minDiff;
+    MinDiffInBST(root.left);
+
+    if (prev != null)
+      minDiff = Math.Min(minDiff, root.val-prev.val);
+
+    prev = root;
+    MinDiffInBST(root.right);
+    return minDiff;
+  }
+}
