36a8e0ffdfbee618cb80bbe6184d4c8c3ef46870
Atiq Rahman <atiqcx@gmail.com>
2018-08-19 21:58:20 -0700

    flatten binary tree to linked list updated

--- general-solving/leetcode/0114_flatten-binary-tree-to-linked-list.cs
+++ general-solving/leetcode/0114_flatten-binary-tree-to-linked-list.cs
@@ -1,32 +1,28 @@
 /***************************************************************************
-* Problem Name: Flatten Binary Tree to Linked List
-* Problem URL : https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
-* Date        : Sept 26 2015
-* Complexity  : 
-* Author      : Atiq Rahman
-* Status      : Accepted
-* Notes       : Key here is that the flattened tree, each node's right child
-*               points to the next node of a pre-order traversal
-* meta        : tag-binary-tree; tag-recursion
+* Title : Flatten Binary Tree to Linked List
+* URL   : https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
+* Date  : 2015-09-26 (update InnoWorld 2018-08-19)
+* Author: Atiq Rahman
+* Comp  : O(n), O(1) O(lg N) stack space
+* Status: Accepted
+* Notes : Key here is that the flattened tree, each node's right child
+*   points to the next node of a pre-order traversal
+*   ToDo: Iterative version
+* meta  : tag-binary-tree, tag-recursion, tag-leetcode-medium
 ***************************************************************************/
-public class Solution
-{
-    TreeNode previousNode;
-    public void Flatten(TreeNode root) {
-        previousNode = null;
-        FlattenRec(root);
+public class Solution {
+  TreeNode previousNode = null;
+  public void Flatten(TreeNode root)
+  {
+    if (root == null)
+      return ;
+    if (previousNode != null) {
+      previousNode.right = root;
+      previousNode.left = null;
     }
-
-    void FlattenRec(TreeNode root) {
-        if (root == null)
-            return;
-        if (previousNode != null) {
-            previousNode.right = root;
-            previousNode.left = null;
-        }
-        previousNode = root;
-        TreeNode temp = root.right;
-        FlattenRec(root.left);
-        FlattenRec(temp);
-    }
+    previousNode = root;
+    TreeNode temp = root.right;
+    Flatten(root.left);
+    Flatten(temp);
+  }
 }
