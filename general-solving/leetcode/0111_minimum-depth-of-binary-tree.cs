c12565a0536854c1d5cfcc6b2e49dbebe0489ec6
Atiq Rahman <atiqcx@gmail.com>
2018-09-06 19:24:57 -0700

    minor: formatting

--- general-solving/leetcode/0111_minimum-depth-of-binary-tree.cs
+++ general-solving/leetcode/0111_minimum-depth-of-binary-tree.cs
@@ -1,28 +1,26 @@
 /***************************************************************************
-*   Problem Name:   Find the minimum depth of a binary tree
-*   Problem URL :   https://leetcode.com/problems/minimum-depth-of-binary-tree/
-*                   http://www.lintcode.com/en/problem/minimum-depth-of-binary-tree/
-*   Date        :   July 22, 2015
+* Title : Find the minimum depth of a binary tree
+* URL   : https://leetcode.com/problems/minimum-depth-of-binary-tree/
+* Date  : 2015-07-22
+* Author: Atiq Rahman
+* Comp  : O(lg N)
+* Status: Accepted
+* Notes : Consider the case when left or right child does not exist.
+*   We cannot take 0 as minimum height for those left/right sides, to ignore
+*   those we use Math.Max
 *
-*   Algo, DS    :   Binary Tree, Recursion
-*   Desc        :   Consider the case when left or right child does not exist,
-*                       we cannot take 0 height as minimum for those parts, to ignore those we use Math.Max
-*   Complexity  :   O(n)
-*   Author      :   Atiq Rahman
-*   Status      :   Accepted
-*   Notes       :   We just adapted C#,
-*                    Learned to use Math.Min
-*                    Moving to C# seems prospective, code is much more readable
+* Rel   : http://www.lintcode.com/en/problem/minimum-depth-of-binary-tree/
+* meta  : tag-binary-tree, tag-recursion
 ***************************************************************************/
-
 public class Solution {
-    public int MinDepth(TreeNode root) {
-        if (root == null)
-            return 0;
-            
-        if (root.left == null || root.right == null)
-            return Math.Max(MinDepth(root.left), MinDepth(root.right)) + 1;
-        
-        return Math.Min(MinDepth(root.left), MinDepth(root.right)) + 1;
-    }
+  public int MinDepth(TreeNode root)
+  {
+    if (root == null)
+      return 0;
+
+    if (root.left == null || root.right == null)
+      return Math.Max(MinDepth(root.left), MinDepth(root.right)) + 1;
+    
+    return Math.Min(MinDepth(root.left), MinDepth(root.right)) + 1;
+  }
 }
