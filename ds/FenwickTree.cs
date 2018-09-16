fb9250119a72995fc3336fe47d80924c290d0dbc
Atiq Rahman <atiqcx@gmail.com>
2018-09-16 16:14:52 -0700

    Fenwick/BIT related problem solved

---
+++ ds/FenwickTree.cs
@@ -0,0 +1,98 @@
+﻿/***************************************************************************
+* Title : Binary Indexed Tree
+* URL   : http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.14.8917&
+*   rep=rep1&type=pdf
+* Date  : 2018-09
+* Author: Atiq Rahman
+* Comp  : O(lg N)
+* Notes : Why Fenwick Tree (BIT)?
+*   Can update and compute prefix sum in lg N time. Uses less memory than RMQ
+*   
+*   1 based index, 0 is the root of all nodes, not touched
+*   Current implementation uses an array of long to pass tests with large
+*   numbers (https://codeforces.com/contest/296/problem/C)
+*   
+* Ref   : SOFTWARE—PRACTICE AND EXPERIENCE, VOL. 24(3), 327–336 (MARCH 1994)
+*   "A New Data Structure for Cumulative Frequency Tables" by peter m. fenwick
+*   In Paper link above we got a nice figure of a tree.
+*   https://www.topcoder.com/community/data-science/data-science-tutorials/
+*   binary-indexed-trees/ provides an example how a sum is computed for
+*   index = 13, [13] + [12] + [8]. Example, uses two arrays f and c.
+*   Practically we might be able to do only with c. tree[idx] is c[i] in our
+*   implementation.
+* meta  : tag-tree
+***************************************************************************/
+using System;
+
+public class FenwickTree {
+  long[] nums;
+  int Size;
+
+  public FenwickTree(int n) {
+    Size = n+1;
+    nums = new long[Size];
+  }
+
+  private int LSB(int x) { return x & -x; }
+  // Lookup cumulative value
+  public long Sum(int i) {
+    if (i< 0 || i > Size)
+      return -1;
+    long sum = 0;
+    while (i > 0) {
+      sum += nums[i];
+      i &= (i-1);
+    }
+    return sum;
+  }
+
+  public void Add(int i, long k) {
+    if (i<1)
+      throw new ArgumentException();
+    while (i < Size) {
+      nums[i] += k;
+      i += LSB(i);
+    }
+  }
+
+  // not used
+  // range based addition of values
+  public void Add(int l, int r, int k) {
+    if (l < 1 || r > Size)
+      throw new ArgumentException();
+    int i = l;
+    while (i<=r) {
+      nums[i] += k;
+      i += LSB(i);
+    }
+    /* this is wrong apprach: because LSB of 9 is not gonna be same as 8's
+    int nextLSB = i;
+    for (i = r + 1; i < Size && i < nextLSB; i++)
+      nums[i] -= k; */
+    // correct approach would be
+    i = r+1;
+    while (i<Size) {
+      nums[i] -= k;
+      i += LSB(i);
+    }
+  }
+
+  public void ShowResult() {
+    var sb = new System.Text.StringBuilder();
+    for (int i = 1; i < Size; i++)
+      sb.Append(" " + Sum(i));
+    Console.WriteLine(sb.ToString());
+  }
+}
+
+/* Debugging stuff..
+public void PrintArray() {
+  Console.WriteLine("vals:");
+  foreach( var num in nums)
+    Console.Write(" " + num);
+  Console.WriteLine();
+  Console.WriteLine("nums array:");
+  for (int i=1; i<Size; i++)
+    Console.Write(" " + Sum(i));
+  Console.WriteLine();
+}*/
---
+++ online-problem-solving/codeforces/296C_Greg_Array.cs
@@ -0,0 +1,92 @@
+﻿/***************************************************************************
+* Title : Greg and Array
+* URL   : https://codeforces.com/contest/296/problem/C
+* Occasn: The Xth Urals Collegiate Programing Championship, March 24-25, 2006
+* Date  : 2018-09-08
+* Author: Atiq Rahman
+* Comp  : O(m lg n) where m is number of operations
+* Status: Accepted
+* Notes : Utilizes two fenwick trees
+*   1. one for queries (precomputes op count)
+*   2. another to apply the queries on original result Fenwick Tree
+*   
+*   long int is required for storing results
+* meta  : tag-tree, tag-fenwick-tree
+***************************************************************************/
+using System;
+using System.Collections.Generic;
+
+class Operation {
+  public int l { get; set; }
+  public int r { get; set; }
+  public int d { get; set; }
+
+  public Operation(int l, int r, int d)
+  {
+    this.l = l;
+    this.r = r;
+    this.d = d;
+  }
+}
+
+// class FenwickTree is at 'ds/FenwickTree.cs'
+
+public class FenwickTreeDemo {
+  public void Run() {
+    string[] tokens = Console.ReadLine().Split();
+    int n = int.Parse(tokens[0]);
+    int m = int.Parse(tokens[1]);
+    int k = int.Parse(tokens[2]);
+
+    tokens = Console.ReadLine().Split();
+
+    FenwickTree result = new FenwickTree(n);
+    // add initial values
+    // nums = new int[n];
+    for (int i = 0; i < n; i++) {
+      // nums[i] = int.Parse(tokens[i]);
+      int v = int.Parse(tokens[i]);
+      result.Add(i + 1, v);   // BIT 1 based index
+      result.Add(i + 2, -v);
+    }
+    // result.PrintArray();
+
+    Operation[] ops = new Operation[m];
+    for (int i = 0; i < m; i++) {
+      tokens = Console.ReadLine().Split();
+      if (tokens.Length != 3)
+        throw new ArgumentException();
+      ops[i] = new Operation(int.Parse(tokens[0]), int.Parse(tokens[1]), int.Parse(tokens[2]));
+    }
+
+    FenwickTree opCount = new FenwickTree(m);
+    for (int i = 0; i < k; i++) {
+      tokens = Console.ReadLine().Split();
+      int l = int.Parse(tokens[0]);
+      int r = int.Parse(tokens[1]);
+      // do for whole tree
+      opCount.Add(l, 1);
+      // undo for r to n
+      opCount.Add(r+1, -1);
+    }
+
+    // at this point, opCount contains info which operation should be applied
+    // how many times
+
+    // for each operation apply them opCount times
+    for (int i = 0; i < m; i++) {
+      long v = ops[i].d * opCount.Sum(i+1);
+      result.Add(ops[i].l, v);
+      result.Add(ops[i].r+1, -v);
+    }
+    // result.PrintArray();
+    result.ShowResult();
+  }
+}
+
+class Timus_Solution {
+  static void Main(String[] args) {
+    FenwickTreeDemo Demo = new FenwickTreeDemo();
+    Demo.Run();
+  }
+}
