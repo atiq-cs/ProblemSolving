6761f1f65ce44b40f5ab53d01b6bd6d120492277
Atiq Rahman <atiqcx@gmail.com>
2018-10-06 11:05:39 -0700

    Adding Visual Studio Support for source control** and code browsing in the Repo VS .gitignore added for Workspace dir Main.cs is a default template inherited from codeforces

---
+++ Workspace/Main.cs
@@ -0,0 +1,36 @@
+﻿/***************************************************************************
+* Title : Prob Name
+* URL   : https://codeforces.com/xxx
+* Occasn: Contest name or an event
+* Date  : 2018-mm-dd
+* Author: Atiq Rahman
+* Comp  : O(X)
+* Status: W/A/CE
+* Notes : desc
+* Ref   :
+* meta  : tag-
+***************************************************************************/
+using System;
+using System.Collections.Generic;
+
+public class ProbDemo {
+  int n;
+
+  public void Run() {
+    n = int.Parse(Console.ReadLine());
+    string str = Console.ReadLine();
+    Console.WriteLine(GetResult() + str);
+  }
+
+  private string GetResult() {
+    // do compute
+    return "echoed ";
+  }
+}
+
+class CFSolution {
+  static void Main(String[] args) {
+    ProbDemo demo = new ProbDemo();
+    demo.Run();
+  }
+}
