acac50cca0d03d0f5644d38df370e9c25747957c
Atiq Rahman <atiqcx@gmail.com>
2018-10-06 17:42:35 -0700

    sql problems added

---
+++ general-solving/hackerrank/sql/AdvancedSelect.sql
@@ -0,0 +1,24 @@
+/***************************************************************************
+* Title : Advanced Select
+* URL   : hckrrnk.com/domains/sql?filters%5Bsubdomains%5D%5B%5D=advanced-select
+* Date  : 2018-06
+* Author: Atiq Rahman
+* Status: Accepted
+* Notes : MS SQL, 1 solved, 3 more to go
+* meta  : tag-sql
+***************************************************************************/
+/* 01 the-pads
+ * first part: an alphabetically ordered list of all names in OCCUPATIONS,
+ * immediately followed by the first letter of each profession as a
+ * parenthetical
+ * second part: Query the number of ocurrences of each occupation in
+ * OCCUPATIONS. Sort the occurrences in ascending order. */
+Select CONCAT(Name, '(', LEFT(Occupation,1), ')')
+from OCCUPATIONS Order By Name;
+
+With OccupTable As (
+  Select Count(*) as NumOccup, Occupation as mOccupation
+  from OCCUPATIONS
+  Group By Occupation)
+Select CONCAT('There are a total of ', NumOccup, ' ', LOWER(mOccupation), 's.')
+From OccupTable Order By NumOccup, mOccupation;
---
+++ general-solving/hackerrank/sql/BasicJoin.sql
@@ -0,0 +1,49 @@
+/***************************************************************************
+* Title : Basic Join
+* URL   : hackerrank.com/domains/sql?filters%5Bsubdomains%5D%5B%5D=join
+* Date  : 2017-09
+* Author: Atiq Rahman
+* Status: Accepted
+* Notes : MS SQL, 3 easy solved, 4 medium to go
+* meta  : tag-sql
+***************************************************************************/
+/* 01 asian-population
+ * Given the CITY and COUNTRY tables, query the sum of the populations of all
+cities where the CONTINENT is 'Asia'. */
+Select sum(Population)
+From City
+Where CountryCode IN (
+  Select CountryCode
+  From City, Country
+  Where City.CountryCode=Country.Code AND Continent='Asia'
+);
+
+/* 02 african-cities
+ * Given the CITY and COUNTRY tables, query the names of all cities where the
+CONTINENT is 'Africa'. */
+Select Name
+From City
+Where CountryCode IN (
+  Select CountryCode
+  From City, Country
+  Where City.CountryCode=Country.Code AND Continent='Africa'
+);
+
+/* 03 average-population-of-each-continent
+ * Given the CITY and COUNTRY tables, query the names of all the continents
+(COUNTRY.Continent) and their respective average city populations
+(CITY.Population) rounded down to the nearest integer. */
+Select Continent, FLOOR(avg(City.Population))
+From City, Country
+Where City.CountryCode=Country.Code
+Group By Continent;
+
+Select hacker_id, name
+from Hackers
+WHERE hacker_id IN (
+  Select hacker_id
+  from Challenges, Submissions, Difficulty
+  WHERE Challenges.challenge_id=Submissions.challenge_id AND Difficulty.
+    difficulty_level=Challenges.difficulty_level AND Challenges.Score=
+    Submissions.Score
+);
