1592ec1c14e747aeb1459bafd7d8e055e33b3e5b
Atiq Rahman <atiqcx@gmail.com>
2018-10-08 14:45:01 -0700

    Note formatting update

--- algo/Graph/03_dijkstra.cpp
+++ algo/Graph/03_dijkstra.cpp
@@ -1,19 +1,18 @@
 /*
-For now, refer to: online-problem-solving\spoj\SPOJ_TRVCOST.cpp
-A generic implementation deomonstration will be added lter
+Note: For now, please refer to: online-problem-solving\spoj\23132_TRVCOST.cpp
+  A generic implementation deomonstration will be added lter
 
 Issues to fix:
-    A use of map implementation instead of 2d array for maintaining cost which will be useful for 
-    sparse graphs
+  - A use of map implementation instead of 2d array for maintaining cost which
+  will be useful for sparse graphs
+  - there are a number of redundant operations in code
+  - Two vectors had been initialized with provided size
+  - Then, they have been cleared
+     vertex.clear();
+     adj_list.clear();
+   Afterwards, they have been pushed again
+   Though it saves from memory reallocation overhead. However, these are
+   redundant.
 
-    ** there are a number of redundant operations in code
-    Two vectors had been initialized with provided size
-    Then they have been cleared
-        vertex.clear();
-        adj_list.clear();
-    Afterwards, they have been pushed again
-    Though it saves from memory reallocation overhead.
-    However, these are sort of redundant operations
-
-    BFS implementation will avoid them
+  BFS implementation should avoid avoid these.
 */
