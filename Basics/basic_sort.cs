4a88b1ce21892662e58e6a32df1238b3926988c5
Atiq Rahman <atiqcx@gmail.com>
2018-10-15 22:55:17 -0700

    all docu style inside basic sort source file updated

--- Basics/basic_sort.cs
+++ Basics/basic_sort.cs
@@ -1,23 +1,22 @@
-/*
- * Problem   : Sorting Algorithms
- * Author    : Atiq Rahman
- * Date      : Sep 23, 2015
- * Desc      : Algos covered,
- *              Selection sort
- *              Bubble sort
- *              Insertion sort
- * meta      : tag-sorting 
- */
-
+/***************************************************************************************************
+* Title : Sorting Algorithms
+* Author: Atiq Rahman
+* Date  : 2015-09-23
+* Notes : Algos covered,
+*   Selection sort
+*   Bubble sort
+*   Insertion sort
+* meta  : tag-algo-sort, tag-algo-core
+***************************************************************************************************/
 using System;
 
 class SortingAlgorithms {
   /*
-  * Algo      : Selection sort (Select Minimum)
-  * Desc      : Finds the smallest number each time and put it in front of
-  *             unsorted window. It's as if the selected window is reduced by
-  *             one each time starting with length n
-  * Complexity: Worst case, average case and best case - O (n^2)
+  * Algo  : Selection sort (Select Minimum)
+  * Notes : Finds the smallest number each time and put it in front of
+  *   unsorted window. It's as if the selected window is reduced by
+  *   one each time starting with length n
+  * Comp  : Worst case, average case and best case - O(n^2)
   */
   public void SelectionSort(int[] A) {
     for (int i = 0; i < A.Length - 1; i++) {
@@ -28,15 +27,14 @@ class SortingAlgorithms {
       Swap<int>(ref A[iMin], ref A[i]);
     }
   }
-
   /*
-  * Algo      : Bubble sort (Select Maximum with Continuous Swapping if we call it bubbling)
-  * Desc      : Finds the maximum each time and put it on appropriate index swapping all of them
-  *             cocktail shaker sort evolves from this
-  * Complexity: Worst case and average case - O (n^2)
-  *             Best case O(n)
-  * Versions  : v2 - adds isSwap
-  *             v3 - updates n based last swapped happened (instead of isSwapped)
+  * Algo  : Bubble sort (Select Maximum with Continuous Swapping if we call it bubbling)
+  * Notes : Finds the maximum each time and put it on appropriate index swapping all of them
+  *   cocktail shaker sort evolves from this
+  * Comp  : Worst case and average case - O (n^2); Best case O(n)
+  * Versions:
+  *   v2 - adds isSwap
+  *   v3 - updates n based last swapped happened (instead of isSwapped)
   */
   public void BubbleSort_v1(int[] A) {
     int n = A.Length;
@@ -64,22 +62,21 @@ class SortingAlgorithms {
       n--;
     } while (isSwapped);
   }
-
   /*
-  * Algo      : Insertion sort (Reverse Bubble)
-  * Desc      : Takes an index (i) from beginning to end
-                Start swapping from i to beginning of the array as long as swap is possible in reverse
-                direction. As a result, after each iteration items of the array upto i is sorted
-                For example, consider the array 5, 4, 3, 8, 7, 6
-                in step 1, i=1, it will sort: 4, 5
-                i=2, it will sort, 3, 4, 5
-                version 2 does not do swap but only moves items 
-                As if items are being inserted in their proper position in the sorted list each time
-                Ref: C.L.R.S p#18 (shows loop invariants and corrections), elaborate runtime analysis:
-                p#26
-
-  * Complexity: Worst case and average case - O (n^2)
-  *             Best case Ω(n)
+  * Algo  : Insertion sort (Reverse Bubble)
+  * Notes : Takes an index (i) from beginning to end
+  *   Start swapping from i to beginning of the array as long as swap is possible in reverse
+  *   direction. As a result, after each iteration items of the array upto i is sorted
+  *   For example, consider the array 5, 4, 3, 8, 7, 6
+  *   in step 1, i=1, it will sort: 4, 5
+  *   i=2, it will sort, 3, 4, 5
+  *   version 2 does not do swap but only moves items
+  *   As if items are being inserted in their proper position in the sorted list each time
+  *   Ref: C.L.R.S p#18 (shows loop invariants and corrections), elaborate runtime analysis:
+  *   p#26
+  *
+  * Comp  : Worst case and average case - O (n^2)
+  *   Best case Ω(n)
   */
   public void InsertionSort_v1(int[] A) { // slightly improved version: swaps eliminated
     for (int i = 1; i < A.Length; i++) {
