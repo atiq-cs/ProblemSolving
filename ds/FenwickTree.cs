958b564d8bcdf779b0d2aa2ece1fe235ea33bde3
Atiq Rahman <atiqcx@gmail.com>
2018-10-26 22:13:25 -0700

    fenwick tree problem solved  - better explanation of methods ds-core updated

--- ds/FenwickTree.cs
+++ ds/FenwickTree.cs
@@ -1,74 +1,132 @@
-/***************************************************************************
+/***************************************************************************************************
 * Title : Binary Indexed Tree
-* URL   : http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.14.8917&
-*   rep=rep1&type=pdf
+* URL   : http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.14.8917&rep=rep1&type=pdf
 * Date  : 2018-09
 * Author: Atiq Rahman
 * Comp  : O(lg N)
 * Notes : Why Fenwick Tree (BIT)?
-*   - Can update and compute prefix sum in lg N time.
+*   - Can update and compute prefix sum in lg N time
 *   - Uses less memory than RMQ
 *   
-*   In this implementation, 1 based index, 0 is the root of all nodes, not
-*   touched
-*   Current implementation uses an array of long to pass tests with large
-*   numbers (https://codeforces.com/contest/296/problem/C)
+*   In the beginning implementation I did was 1 based index, 0 was the root of all nodes, not
+*   touched. Then, I updated it to be 0 based. See method documentation inline.
+*   
+*   Current implementation uses an array of long to pass tests with large numbers
+*     (https://codeforces.com/contest/296/problem/C)
 *   
 * Ref   : SOFTWARE—PRACTICE AND EXPERIENCE, VOL. 24(3), 327–336 (MARCH 1994)
 *   "A New Data Structure for Cumulative Frequency Tables" by peter m. fenwick
-*   In Paper link above we got a nice figure of an example BIT.
+*   In Paper link (p#3) above we got a nice figure of an example BIT.
 *
-*   https://www.topcoder.com/community/data-science/data-science-tutorials/
-*   binary-indexed-trees/ provides an example how a sum is computed for
-*   index = 13, [13] + [12] + [8]. Example, uses two arrays f and c.
-*   Practically we might be able to do only with c. tree[idx] is c[i] in our
-*   implementation.
+*   https://www.topcoder.com/community/data-science/data-science-tutorials/binary-indexed-trees/
+*   provides an example how a sum is computed for index = 13, [13] + [12] + [8]. Example, uses two
+*   arrays f and c.
+*   Practically we might be able to do only with c. tree[idx] is c[i] in our implementation.
 *
-*   https://sanugupta.wordpress.com/2014/08/29/binary-indexed-tree-fenwick-tree/
-*   average article on the topic
-* meta  : tag-tree
-***************************************************************************/
+*   https://sanugupta.wordpress.com/2014/08/29/binary-indexed-tree-fenwick-tree/ average article on
+*   the topic
+* meta  : tag-fenwick-tree, tag-binary-indexed-tree, tag-ds-core
+***************************************************************************************************/
 using System;
 
 public class FenwickTree {
-  long[] nums;
-  int Size;
+  int[] accumNums;    // change to long if bigger range is required
+  // original input numbers array
+  int[] nums;
 
-  public FenwickTree(int n) {
-    Size = n+1;
-    nums = new long[Size];
+  // auxillary methods
+  private int LSB(int x) { return x & -x; }
+
+  /// <summary>
+  /// Build/Init data structure for given array.
+  /// Please note that input array is not copied straight forward. Instead, accumNums is updated
+  /// to contain cumulative sum index by index. In the end, nums property contains the updated
+  /// values.
+  /// </summary>
+  /// <param name="a">array of input numbers</param>
+  public FenwickTree(int[] a) {
+    this.accumNums = new int[a.Length];    // change to long if required
+    this.nums = new int[accumNums.Length];
+    for (int i = 0; i < accumNums.Length; i++)
+      Update(i, a[i]);
   }
 
-  private int LSB(int x) { return x & -x; }
-  // Lookup cumulative value
-  public long Sum(int i) {
-    if (i< 0 || i > Size)
-      return -1;
-    long sum = 0;
+  /// <summary>
+  /// Updating value at an index means updating cumulative values for indices following i (after i).
+  /// For BIT, we update the siblings which takes of updating in lg N time for all following indices
+  /// 
+  /// To get the sibling of i, we do,
+  ///   i = i + LSB(i)
+  /// Example,  
+  /// When i = 1, sibling is 2 (ref: Paper, p#3). Hence, LSB(i) = 2-1 = 1
+  ///
+  /// <remarks> To avoid an infinite loop when i=0 we initialize it to be off by 1 and use
+  /// respective previous index. Please note that changing LSB to,
+  /// <code> ((x+1) & -(x+1)) - 1 </code>
+  /// does not make a difference.
+  /// </remarks>  
+  /// </summary>
+  /// <param name="i">Index at which to update the value</param>
+  /// <param name="val">New value for specified index</param>
+  public void Update(int i, int val) {  // change to long if required
+    if (i < 0)
+      throw new ArgumentException();
+    int k = val - nums[i];
+    nums[i] = val;
+    i++;
+    while (i <= accumNums.Length) {
+      accumNums[i-1] += k;
+      i += LSB(i);
+    }
+  }
+
+  /// <summary>
+  /// Get cumulative sum of values from index 0 to i.
+  /// This means gettings sum of parent nodes one by one all the way above till i=0 since 0 is the
+  /// root of tree. Beautiful bitwise optimization here is to drop the least significant bit annd
+  /// get the parent of a node.
+  /// For example, to get Sum for index 15, we have following order of parents (ref: paper, p#3),
+  /// - 15
+  /// - 14
+  /// - 12
+  /// - 8
+  /// - 0
+  /// 
+  /// when i = 15 = 0b00001111, -15 = 0b11110001
+  /// Hence, LSB(i) = 1, subtract this from 15 and we get the parent, 15 - 1 = 14
+  /// A short expression is to AND with (i-1).
+  /// 
+  /// <remarks> To avoid an infinite loop when i=0 we initialize it to be off by 1 and use
+  /// respective previous index. Please note that changing LSB to,
+  /// <code> ((x+1) & -(x+1)) - 1 </code>
+  /// does not make a difference.
+  /// </remarks>  
+  /// </summary>
+  /// <param name="i">Index</param>
+  public int Sum(int i) {
+    if (i < 0 || i >= accumNums.Length)
+      return 0;
+    i++;
+    int sum = 0;    // change to long if required
     while (i > 0) {
-      sum += nums[i];
+      sum += accumNums[i-1];
       i &= (i-1);
     }
     return sum;
   }
 
-  public void Add(int i, long k) {
-    if (i<1)
-      throw new ArgumentException();
-    while (i < Size) {
-      nums[i] += k;
-      i += LSB(i);
-    }
+  public int SumRange(int i, int j) {
+    return Sum(j) - Sum(i - 1);
   }
 
-  // not used
+  // not tested, stil 1 based
   // range based addition of values
-  public void Add(int l, int r, int k) {
+  public void RangeUpdate(int l, int r, int k) {
     if (l < 1 || r > Size)
       throw new ArgumentException();
     int i = l;
     while (i<=r) {
-      nums[i] += k;
+      accumNums[i] += k;
       i += LSB(i);
     }
     /* this is wrong apprach: because LSB of 9 is not gonna be same as 8's
@@ -78,7 +136,7 @@ public class FenwickTree {
     // correct approach would be
     i = r+1;
     while (i<Size) {
-      nums[i] -= k;
+      accumNums[i] -= k;
       i += LSB(i);
     }
   }
---
+++ general-solving/leetcode/0307_range-sum-query-mutable.cs
@@ -0,0 +1,27 @@
+/***************************************************************************************************
+* Title : Range Sum Query - Mutable
+* URL   : range-sum-query-mutable
+* Date  : 18-10-26
+* Author: Atiq Rahman
+* Comp  : O(lg N)
+* Status: Accepted
+* Notes : Straight forward range sum query problem solved during discussion with Mak
+* ref   : ds/FenwickTree.cs
+* rel   : 
+* meta  : tag-fenwick-tree, tag-binary-indexed-tree
+***************************************************************************************************/
+public class Solution
+{
+  // first 3 methods at 'ds/FenwickTree.cs'
+}
+
+/* Helpful input:
+["NumArray","update","sumRange","sumRange","update","sumRange"]
+[[[9,-8]],[0,3],[1,1],[0,1],[1,-3],[0,1]]
+
+Debugging stuff..
+In Update,
+  Console.WriteLine("a at " + i + ": " + a[i]);
+  Console.WriteLine("num at " + (i-1) + ": " + nums[i-1]);
+  Console.WriteLine("u i: " + i);
+*/
