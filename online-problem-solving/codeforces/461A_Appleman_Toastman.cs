68b031a9fc2bbde89416f0a4c8931dac2770f990
Atiq Rahman <atiqcx@gmail.com>
2018-11-19 10:29:52 -0800

    two problems from contest 4

---
+++ online-problem-solving/codeforces/461A_Appleman_Toastman.cs
@@ -0,0 +1,73 @@
+﻿/***************************************************************************************************
+* Title : Appleman and Toastman
+* URL   : https://codeforces.com/problemset/problem/461/A
+* Occasn: Competitive programming, with Lewin Gan; 8VC Venture Cup 2016 - Final Round (Div. 1 Ed)
+*   Contest#4 Group#ylgaGzHfaj
+* Date  : 2018-11-13
+* Comp  : O(n lg n), O(n)
+* Status: Accepted
+* Notes : Find maximum score possible
+*   Optimal strategy is to get n (all) numbers first; then, take (n-1) numbers and the smallest
+*   number in two different groups Gotta cast to bigger type. Example is at bottom.
+*   
+*   Before applying multiplication to handle large results:
+*   Must cast to long in this line to handle large numbers,
+*     sum += (i+2) * (long) A[i];
+*   (this got caught by some large input test-case)
+* meta  : tag-algo-greedy, tag-algo-sort, tag-easy
+***************************************************************************************************/
+using System;
+
+public class Series {
+  int[] A;
+
+  public void TakeInput() {
+    int.Parse(Console.ReadLine());
+    A = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+  }
+
+  public long GetSum() {
+    Array.Sort(A);
+    long sum = 0;
+    for (int i=0; i < A.Length; i++)
+      sum += (i+2) * (long) A[i];
+    return sum - A[A.Length-1];
+  }
+}
+
+public class CFSolution {
+  private static void Main() {
+    Series demo = new Series();
+    demo.TakeInput();
+    Console.WriteLine(demo.GetSum());
+  }
+}
+
+/*Draft,
+3 1 5
++9 = 5 + 3 + 1
++8 = 5 + 3 & +1 separately
++5 = 5 & +3 separately
+
+5*3 + 3 * 3 + 1 * 2
+= 15 + 9 + 2
+= 26
+
+5 * 4 + 3 * 3 + 1*2 - 5 = 26
+
+1 2 3 4 5
+then,
+5..1
+5..2 + 1
+5..3 + 2
+5..4 + 3
+5 + 4
+
+5 * 5 + 4 * 5 + 3*4 + 2*3 + 1*2
+
+try for 2,
+3 4
+4 * 3 + 3 * 2  = 21
+if (n==2) sum -= a[n-2]
+return 21-4
+*/
---
+++ online-problem-solving/codeforces/634A_IslandPuzzle.cs
@@ -0,0 +1,61 @@
+﻿/***************************************************************************************************
+* Title : Island Puzzle
+* URL   : https://codeforces.com/problemset/problem/634/A
+* Occasn: Competitive programming, with Lewin Gan; 8VC Venture Cup 2016 - Final Round (Div. 1 Ed)
+*   Contest#4 Group#ylgaGzHfaj
+* Date  : 2018-11-13
+* Comp  : O(n) 171ms, O(1) 31600KB
+* Status: Accepted
+* Notes : It's an easy problem as input numbers are distinct. Find the first number from rotation
+*   output with original array and keep comparing to find if it matches till the end.
+*
+*   Demonstrates ConvertAll with ReadLine
+* meta  : tag-binary-search, tag-two-pointers, tag-easy
+***************************************************************************************************/
+using System;
+
+public class ArrayRotation {
+  int[] A, eA;
+
+  public void TakeInput() {
+    int.Parse(Console.ReadLine());
+    A = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+    eA = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+  }
+
+  // Check if expected order is possible to get by rotation
+  public bool CanGetOrder() {
+    // find position of eA[0] in A
+    int x = eA[0] == 0? eA[1]: eA[0];
+    int firstIndex = 0;
+    for (; firstIndex < A.Length; firstIndex++)
+      if (x == A[firstIndex])
+        break;
+
+    // get possible output for rotation from there
+    int i = (eA[0]==0)?2:1;
+    int rIndex = (firstIndex + 1) % A.Length;
+
+    for (; i < A.Length; i++) {
+      if (eA[i] == 0)
+        continue;
+
+      if (A[rIndex] == 0)
+        rIndex = (rIndex+1) % A.Length;
+      if (eA[i] != A[rIndex])
+        return false;
+
+      rIndex = (rIndex + 1) % A.Length;
+    }
+
+    return true;
+  }
+}
+
+public class CFSolution {
+  private static void Main() {
+    ArrayRotation arDemo = new ArrayRotation();
+    arDemo.TakeInput();
+    Console.WriteLine(arDemo.CanGetOrder() ? "YES" : "NO");
+  }
+}
