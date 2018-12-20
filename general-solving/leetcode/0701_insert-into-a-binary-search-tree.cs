8940b7427f09ba9ce9ffd2a0acbb95c22b594336
Atiq Rahman <atiqcx@gmail.com>
2018-12-20 10:39:51 -0800

    record added for this problem

---
+++ general-solving/leetcode/0701_insert-into-a-binary-search-tree.cs
@@ -0,0 +1,12 @@
+/***************************************************************************
+* Title : Insert Node in a BST
+* URL   : https://leetcode.com/problems/insert-into-a-binary-search-tree
+* Date  : 2018-11
+* Author: Atiq Rahman
+* Comp  : O(h), h = lg n
+* Status: Accepted
+* Notes : there may exist multiple valid ways for the insertion, as long as the
+*   tree remains a BST after insertion. You can return any of them
+* ref   : 'ds/bst/basic_ops.cs'
+* meta  : tag-ds-bst, tag-ds-core
+***************************************************************************/
