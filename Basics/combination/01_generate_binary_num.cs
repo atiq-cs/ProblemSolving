8376f30785ae80d6474c029727528bd9bb517b1f
Atiq Rahman <atiqcx@gmail.com>
2019-01-19 21:45:16 -0800

    number generation examples added

---
+++ Basics/combination/01_generate_binary_num.cs
@@ -0,0 +1,85 @@
+﻿/***************************************************************************************************
+* Title : Generate Binary Number
+* URL   : C.L.R.S defines combinations p#1185
+* Occasn: 
+* Date  : 2019-01
+* Comp  : O(2^N), O(N) Stack
+* Status: 
+* Notes : This is a variation of combination. A combination doesn't repeat subsets. For example,
+*   {0, 1, 0} and {1, 0, 0} can be two different binary numbers.
+*   This does not map to permutation as well. Since, permutation doesn't allow repeating an element
+*   from the Set. Example: {a, b, c}
+*    abc
+*    acb
+*    bac
+*    bca
+*    ...
+*   
+*   This is here for example, not exactly combination.
+* meta  : tag-combination
+***************************************************************************************************/
+using System;
+
+public class NumberSystem {
+  int[] A;
+  int limit;
+
+  public void TakeInput() {
+    limit = int.Parse(Console.ReadLine());
+    A = new int[limit];
+  }
+
+  private void PrintNumber() {
+    for (int i = 0; i < limit; i++)
+      Console.Write(A[i]);
+    Console.WriteLine();
+  }
+
+  public void GenerateBinary(int i=0) {
+    if (i == limit)
+      PrintNumber();
+    else {
+      A[i] = 0;
+      GenerateBinary(i + 1);
+      A[i] = 1;
+      GenerateBinary(i+1);
+    }
+  }
+}
+
+public class Solution {
+  private static void Main() {
+    NumberSystem demo = new NumberSystem();
+    demo.TakeInput();
+    demo.GenerateBinary();
+  }
+}
+
+/*
+Input: 3
+
+Output:
+000
+001
+010
+011
+100
+101
+110
+111
+
+2015-11 version passes an Array of spcified of length instead of passing limit,
+
+  public void comb(int[] A, int k) {
+    if (k == A.Length) {
+      DisplayArray(A);
+      return;
+    }
+    // This can be replaced with a loop
+    A[k] = 0;
+    comb(A, k + 1);
+    A[k] = 1;
+    comb(A, k + 1);
+  }
+
+*/
---
+++ Basics/combination/02_generate_num_general_base.cs
@@ -0,0 +1,68 @@
+﻿/***************************************************************************************************
+* Title : Generate Numbers for provided base
+* URL   : C.L.R.S defines combinations p#1185
+* Occasn: 
+* Date  : 2019-01
+* Comp  : 
+* Status: 
+* Notes : See '01_generate_binary_num.cs' for more details
+*   
+*   This is here for example, not exactly combination.
+*   However, recursion loop is very identical to combination's loop
+*   
+*   Example output at bottom
+* meta  : tag-combination, tag-recursion
+***************************************************************************************************/
+using System;
+
+public class NumberSystem {
+  int[] A;
+  int maxDigits;
+  int baseNum;
+
+  public void TakeInput() {
+    maxDigits = int.Parse(Console.ReadLine());
+    A = new int[maxDigits];
+    baseNum = int.Parse(Console.ReadLine());
+  }
+
+  private void PrintNumber() {
+    for (int i = 0; i < maxDigits; i++)
+      Console.Write(A[i]);
+    Console.WriteLine();
+  }
+
+  public void Generate(int i=0) {
+    if (i == maxDigits)
+      PrintNumber();
+    else
+      for (int k=0; k<baseNum; k++) {
+        A[i] = k;
+        Generate(i + 1);
+      }
+  }
+}
+
+public class Solution {
+  private static void Main() {
+    NumberSystem demo = new NumberSystem();
+    demo.TakeInput();
+    demo.Generate();
+  }
+}
+
+/*
+Input:
+2 (max digits)
+3 (base)
+Output:
+00
+01
+02
+10
+11
+12
+20
+21
+22
+*/
