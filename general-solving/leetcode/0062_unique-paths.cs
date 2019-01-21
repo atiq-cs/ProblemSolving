d73461882833c1f2af08d54522e840b6b0d20484
Atiq Rahman <atiqcx@gmail.com>
2019-01-20 19:13:43 -0800

    a DP problem similar to Fortress IQ second one (counting paths toward bottom right) solved

---
+++ general-solving/leetcode/0062_unique-paths.cs
@@ -0,0 +1,45 @@
+/***************************************************************************************************
+* Title : Unique Paths
+* URL   : https://leetcode.com/problems/unique-paths/
+* Date  : 2019-01-20
+* Comp  : O(mn), O(n)
+* Status: Accepted
+* Notes : Easier version of FortressIQ 2nd problem, hence found it too easy
+* rel   : 'general-solving/coding-tests/FortressIQ_2.cs'
+* meta  : tag-algo-dp, tag-leetcode-medium
+***************************************************************************************************/
+public class Solution {
+  public int UniquePaths(int m, int n) {
+    int[] prevRow = new int[n];   // just previous row
+    for (int i = 0; i < n; i++)
+      prevRow[i] = 1;
+    int prevCol = 1;
+
+    for (int row = 1; row < m; row++)
+      for (int col = 1; col < n; col++) {
+        prevRow[col] = prevRow[col] + prevCol;
+        prevCol = col==n-1?1:prevRow[col];
+      }
+
+    return prevRow[n - 1];
+  }
+
+  // initial easy version, O(mn), O(mn)
+  public int UniquePaths_v1(int m, int n) {
+    // Allocation and initialization
+    int[][] pathCount = new int[m][];
+    for (int i = 0; i < m; i++) {
+      pathCount[i] = new int[n];
+      pathCount[i][0] = 1;
+    }
+    for (int i = 0; i < n; i++)
+      pathCount[0][i] = 1;
+
+    // DP
+    for (int row = 1; row < m; row++)
+      for (int col = 1; col < n; col++)
+        pathCount[row][col] = pathCount[row - 1][col] + pathCount[row][col - 1];
+
+    return pathCount[m - 1][n - 1];
+  }
+}
